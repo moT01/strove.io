6e18408d0b0e1a727acda8a807e2fc51e11229b4
adamzaczek <adamzaczekul@gmail.com>
2019-06-07 16:53:29 +0200

    Make a fancy iframe

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,15 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const StyledIframe = styled.iframe`
+  display: block;
+  background: #000;
+  border: none;
+  min-height: 80vh;
+  width: 100vw;
+`
+
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <iframe src="https://dmb9kya1j9.execute-api.eu-central-1.amazonaws.com/development/editor" />
+    <StyledIframe src="https://dmb9kya1j9.execute-api.eu-central-1.amazonaws.com/development/editor" />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
