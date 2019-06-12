716e381dc08cc5aa19be624603a9fd5d1b70c3af
adamzaczek <adamzaczekul@gmail.com>
2019-06-12 15:33:12 +0200

    Stash changes

--- src/components/login.js
+++ src/components/login.js
@@ -30,66 +30,53 @@ const STATUS = {
 }
 
 const LoginButton = styled.a`
-  -webkit-appearance: none;
-  position: relative;
-  display: inline-block;
-  vertical-align: middle;
-  text-transform: uppercase;
-  text-align: center;
-  line-height: 38px;
-  white-space: nowrap;
-  min-width: 190px;
-  height: 40px;
-  font-weight: 500;
-  font-size: 12px;
-  flex-shrink: 0;
-  color: rgb(255, 255, 255);
-  /* background-color: rgb(36, 41, 46); */
-  user-select: none;
-  cursor: pointer;
+  color: white;
   text-decoration: none;
-  padding: 0px 25px 0px 42px;
-  border-radius: 5px;
-  border-width: 1px;
-  /* border-style: solid;
-  border-color: rgb(36, 41, 46); */
-  border-image: initial;
-  transition: all 0.2s ease 0s;
-  overflow: hidden;
-  outline: none;
-
-  * {
-    vertical-align: bottom;
-  }
 
   :hover {
     color: black;
     text-decoration: none;
   }
+
+  * {
+    vertical-align: bottom;
+  }
 `
 
 const LinkText = styled.span`
-  text-transform: uppercase;
-  text-align: center;
-  line-height: 38px;
-  white-space: nowrap;
-  min-width: 190px;
-  height: 40px;
-  font-weight: 500;
-  font-size: 1.38316rem;
-  flex-shrink: 0;
-  color: rgb(255, 255, 255);
-  /* background-color: rgb(36, 41, 46); */
-  user-select: none;
-  cursor: pointer;
-
+  fill: red;
   :hover {
-    color: black;
+    fill: black;
   }
 `
 
 const Inline = styled.div`
   display: inline-block;
+`
+
+const GitubLogo = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20pt"
+    height="20pt"
+    viewBox="0 0 15 15"
+    version="1.1"
+    // style={{ fill: "white" }}
+  >
+    <path
+      d="M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z"
+      fill="var(--geist-foreground)"
+      fill-rule="nonzero"
+    />
+  </svg>
+)
+
+const StyledGitubLogo = styled(GitubLogo)`
+  :hover {
+    svg {
+      fill: #da4567;
+    }
+  }
 `
 
 class LoginComponent extends PureComponent {
@@ -165,20 +152,7 @@ class LoginComponent extends PureComponent {
       >
         <LinkText>Login </LinkText>
         <Inline>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="25pt"
-            height="25pt"
-            viewBox="0 0 25 25"
-            version="1.1"
-            style={{ fill: "white" }}
-          >
-            <path
-              d="M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z"
-              fill="var(--geist-foreground)"
-              fill-rule="nonzero"
-            />
-          </svg>
+          <StyledGitubLogo />
         </Inline>
       </LoginButton>
     )
