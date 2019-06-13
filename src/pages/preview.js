46fb1ccb27523b3b698cd9450e97b0a166bc55e9
adamzaczek <adamzaczekul@gmail.com>
2019-06-13 20:29:15 +0200

    Start moving error to jsx

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -27,84 +27,82 @@ function useHookWithRefCallback() {
   return [setRef]
 }
 
-const Error = `
-<!doctype html>
-
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>No preview yet :/</title>
-    <meta name="description" content="No preview">
-    <meta name="author" content="Silisky">
-    <link rel="stylesheet" href="./index.css">
-    <style>
-      body {
-        background: #081421;
-        color: #d3d7de;
-        font-family: "Courier new";
-        font-size: 18px;
-        line-height: 1.5em;
-      cursor: default;
-      }
-      a {
-        color: #fff;
-      }
-
-      .code-area {
-          position: absolute;
-         width: 320px;
-        min-width: 320px;
-          top: 50%;
-          left: 50%;
-          -webkit-transform: translate(-50%, -50%);
-          transform: translate(-50%, -50%);
-      }
-
-      .code-area > span {
-          display: block;
-      }
-
-      @media screen and (max-width: 320px) {
-          .code-area {
-          font-size: 5vw;
-          min-width: auto;
-              width: 95%;
-          margin: auto;
-          padding: 5px;
-          padding-left: 10px;
-          line-height: 6.5vw;
-          }
-      }
-    </style>
-  </head>
-  <body>
-    <div class="code-area">
-        <span style="color: #777;font-style:italic;">
-          /* Preview not found. Check console in editor - something probably crashed! */
-        </span>
-        <span>
-          <span style="color:#d65562;">
-            if
-          </span>
-          (<span style="color:#4ca8ef;">!</span><span style="font-style: italic;color:#bdbdbd;">found</span>)
-          {
-        </span>
-        <span>
-          <span style="padding-left: 15px;color:#2796ec">
-            <i style="width: 10px;display:inline-block"></i>throw
-          </span>
-          <span>
-            (<span style="color: #a6a61f">"(╯°□°)╯︵ ┻━┻"</span>);
-          </span>
-          <span style="display:block">}</span>
-          <span style="color: #777;font-style:italic;">
-            // <a href="http://localhost:8000/editor">Go back!</a>
-          </span>
-        </span>
-      </div>
-  </body>
-</html>
+const CodeArea = styled.div`
+  position: absolute;
+  width: 320px;
+  min-width: 320px;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 `
+
+const Error = () => (
+  //   <style>
+  //     body {
+  //       background: #081421;
+  //       color: #d3d7de;
+  //       font-family: "Courier new";
+  //       font-size: 18px;
+  //       line-height: 1.5em;
+  //     cursor: default;
+  //     }
+  //     a {
+  //       color: #fff;
+  //     }
+
+  //     .code-area {
+  //         position: absolute;
+  //        width: 320px;
+  //       min-width: 320px;
+  //         top: 50%;
+  //         left: 50%;
+  //         -webkit-transform: translate(-50%, -50%);
+  //         transform: translate(-50%, -50%);
+  //     }
+
+  //     .code-area > span {
+  //         display: block;
+  //     }
+
+  //     @media screen and (max-width: 320px) {
+  //         .code-area {
+  //         font-size: 5vw;
+  //         min-width: auto;
+  //             width: 95%;
+  //         margin: auto;
+  //         padding: 5px;
+  //         padding-left: 10px;
+  //         line-height: 6.5vw;
+  //         }
+  //     }
+  //   </style>
+  // </head>
+  <CodeArea>
+    <span style="color: #777;font-style:italic;">
+      /* Preview not found. Check console in editor - something probably
+      crashed! */
+    </span>
+    <span>
+      <span style="color:#d65562;">if</span>
+      <span style="color:#4ca8ef;">!</span>
+      <span style="font-style: italic;color:#bdbdbd;">found</span>
+    </span>
+    <span>
+      <span style="padding-left: 15px;color:#2796ec">
+        <i style="width: 10px;display:inline-block" />
+        throw
+      </span>
+      <span>
+        (<span style="color: #a6a61f">"(╯°□°)╯︵ ┻━┻"</span>);
+      </span>
+      <span style="display:block">}</span>
+      <span style="color: #777;font-style:italic;">
+        // <a href="http://localhost:8000/editor">Go back!</a>
+      </span>
+    </span>
+  </CodeArea>
+)
 
 const testToken = "testToken"
 
