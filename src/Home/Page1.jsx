ce1917e30663ae4afd25b4525ddb2117c5989b69
adamzaczek <adamzaczekul@gmail.com>
2019-06-17 14:41:37 +0200

    Update bubbles

--- src/Home/Page1.jsx
+++ src/Home/Page1.jsx
@@ -11,12 +11,12 @@ const { TweenOneGroup } = TweenOne
 
 const featuresCN = [
   {
-    title: "Push straight to GitHub or Bitbucket",
-    content: "SiliSky is designed to work with every cloud",
-    src: "https://gw.alipayobjects.com/zos/rmsportal/VriUmzNjDnjoFoFFZvuh.svg",
-    color: "#1AC44D",
-    shadowColor: "rgba(26,196,77,.12)",
-    type: "cloud",
+    title: "Run projects straight in the browser",
+    content: "No setup needed",
+    src: "https://gw.alipayobjects.com/zos/rmsportal/aLQyKyUyssIUhHTZqCIb.svg",
+    color: "#EB2F96",
+    shadowColor: "rgba(235,45,150,.12)",
+    type: "ie",
   },
   {
     title: "Work with VSCode",
@@ -26,21 +26,21 @@ const featuresCN = [
     shadowColor: "rgba(0, 114, 206, 0.12)",
   },
   {
-    title: "Code in all major programming languages",
-    content:
-      "You can even use a particular version of a language. Need Python 2.7.3 for chemistry? We got you covered!",
-    src: "https://gw.alipayobjects.com/zos/rmsportal/BISfzKcCNCYFmTYcUygW.svg",
-    color: "#722ED1",
-    shadowColor: "rgba(114,46,209,.12)",
-    type: "code",
-  },
-  {
     title: "Access your code anywhere",
     content: "Log in to your account from any computer and code in the cloud",
     src: "https://gw.alipayobjects.com/zos/rmsportal/JsixxWSViARJnQbAAPkI.svg",
     color: "#13C2C2",
     shadowColor: "rgba(19,194,194,.12)",
     type: "global",
+  },
+  {
+    title: "Code in all major programming languages",
+    content:
+      "You can even use a particular language version. Need Python 2.7.3 for chemistry? We got you covered!",
+    src: "https://gw.alipayobjects.com/zos/rmsportal/BISfzKcCNCYFmTYcUygW.svg",
+    color: "#722ED1",
+    shadowColor: "rgba(114,46,209,.12)",
+    type: "code",
   },
   {
     title: "Don't lose time, one environment for the whole team",
@@ -53,7 +53,8 @@ const featuresCN = [
   },
   {
     title: "Build fullstack apps",
-    content: "Anything that works on linux works on SiliSky",
+    content:
+      "and save battery, CPU and RAM. Expensive computations are done in the cloud",
     src: "https://gw.alipayobjects.com/zos/rmsportal/hBbIHzUsSbSxrhoRFYzi.svg",
     color: "#F5222D",
     shadowColor: "rgba(245,34,45,.12)",
@@ -69,16 +70,17 @@ const featuresCN = [
     type: "github",
   },
   {
-    title: "Run projects straight in the browser",
-    content: "You don't need to download anything to get yourself started",
-    src: "https://gw.alipayobjects.com/zos/rmsportal/aLQyKyUyssIUhHTZqCIb.svg",
-    color: "#EB2F96",
-    shadowColor: "rgba(235,45,150,.12)",
-    type: "ie",
+    title: "Push straight to GitHub or Bitbucket",
+    content: "Silisky is made with seamless git experience in mind",
+    src: "https://gw.alipayobjects.com/zos/rmsportal/VriUmzNjDnjoFoFFZvuh.svg",
+    color: "#1AC44D",
+    shadowColor: "rgba(26,196,77,.12)",
+    type: "cloud",
   },
   {
     title: "Say goodbye to 'it works on my machine'",
-    content: "It's gonna work on every machine",
+    content:
+      "Thanks to docker based infrastructure every member of the team works on the same environment",
     src: "https://gw.alipayobjects.com/zos/rmsportal/RpJIQitGbSCHwLMimybX.svg",
     color: "#1890FF",
     shadowColor: "rgba(24,144,255,.12)",
