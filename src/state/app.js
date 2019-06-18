c0d341030cf25119bab4628ca92d210180a02471
adamzaczek <adamzaczekul@gmail.com>
2019-06-18 11:03:50 +0200

    Make simple redux-actions implementation

--- src/state/app.js
+++ src/state/app.js
@@ -1,28 +1,42 @@
-import { createActions, handleActions, combineActions } from "redux-actions"
+// import { createActions, handleActions, combineActions } from "redux-actions"
+
+// const defaultState = { user: null }
+
+// const { getUser } = createActions({
+//   GET_USER: user => ({ ...user }),
+//   // FETCH: {
+//   //   USER: {
+//   //     DATA: user => ({ ...user }),
+//   //     LOADING: (isLoading = false) => ({ isLoading }),
+//   //     ERROR: error => ({ ...error })
+//   //   }
+//   // }
+// })
+
+// const reducer = handleActions(
+//   getUser,
+//   (state, action) => ({ user: action.payload }),
+//   // {
+//   //   [combineActions(increment, decrement)]: (
+//   //     state,
+//   //     { payload: { amount } }
+//   //   ) => {
+//   //     return { ...state, counter: state.counter + amount };
+//   //   }
+//   // },
+//   defaultState
+// )
+
+import { createAction, handleAction } from "redux-actions"
 
 const defaultState = { user: null }
 
-const { getUser } = createActions({
-  GET_USER: user => ({ ...user }),
-  // FETCH: {
-  //   USER: {
-  //     DATA: user => ({ ...user }),
-  //     LOADING: (isLoading = false) => ({ isLoading }),
-  //     ERROR: error => ({ ...error })
-  //   }
-  // }
-})
+const { getUser } = createAction("GET_USER", user => ({ ...user }))
 
-const reducer = handleActions(
-  getUser,
+const reducer = handleAction(
+  "GET_USER",
   (state, action) => ({ user: action.payload }),
-  // {
-  //   [combineActions(increment, decrement)]: (
-  //     state,
-  //     { payload: { amount } }
-  //   ) => {
-  //     return { ...state, counter: state.counter + amount };
-  //   }
-  // },
   defaultState
 )
+
+export default reducer
