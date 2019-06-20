ea733322796954bb1d9445722f29a6b9f136fed5
adamzaczek <adamzaczekul@gmail.com>
2019-06-20 11:42:54 +0200

    Export components from index

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Clouds from "./clouds"
+import Header from "./header"
+import Image from "./image"
+import Login from "./logos"
+import Seo from "./seo"
+
+export { Clouds, Header, Image, Login, Seo }
