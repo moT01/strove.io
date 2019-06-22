b2cc66e6ed071b56afa2fca68cd5f691cc5385a1
adamzaczek <adamzaczekul@gmail.com>
2019-06-22 22:55:37 +0200

    Stash changes

--- src/utils/index.js
+++ src/utils/index.js
@@ -77,17 +77,17 @@ export const query = ({
   }
 }
 
-const createFetchReducers = ({ storeName, initState, data, loading, error }) =>
-  handleActions(
-    {
-      LOGOUT: state => ({
-        ...state,
-        user: {
-          data: null,
-          loading: false,
-          error: null,
-        },
-      }),
+const createFetchReducers = actions => {
+  const args = actions.map(
+    ({
+      storeName,
+      initState = {
+        [storeName]: { loading: false, data: null, error: null },
+      },
+      data,
+      loading,
+      error,
+    }) => ({
       [data]: (state, { payload }) => ({
         ...state,
         [storeName]: {
@@ -117,39 +117,70 @@ const createFetchReducers = ({ storeName, initState, data, loading, error }) =>
           data: { ...state[storeName].data },
         },
       }),
-    },
-    initState
+    })
   )
 
-export const createFetchModule = submodules =>
-  submodules.forEach(({ storeName, initialState }) => {
+  console.log("args", args)
+}
+
+// const createFetchReducers = ({ storeName, initState, data, loading, error }) =>
+//   handleActions(
+//     {
+//       LOGOUT: state => ({
+//         ...state,
+//         user: {
+//           data: null,
+//           loading: false,
+//           error: null,
+//         },
+//       }),
+//       [data]: (state, { payload }) => ({
+//         ...state,
+//         [storeName]: {
+//           loading: false,
+//           error: null,
+//           /* Spread for objects and arrays, assign value directly for primitive */
+//           data: Array.isArray(payload)
+//             ? [...payload]
+//             : typeof payload === "object"
+//             ? { ...state[storeName].data, ...payload }
+//             : payload,
+//         },
+//       }),
+//       [error]: (state, { payload }) => ({
+//         ...state,
+//         [storeName]: {
+//           loading: false,
+//           data: null,
+//           error: payload,
+//         },
+//       }),
+//       [loading]: (state, { payload }) => ({
+//         ...state,
+//         [storeName]: {
+//           loading: payload,
+//           error: null,
+//           data: { ...state[storeName].data },
+//         },
+//       }),
+//     },
+//     initState
+//   )
+
+export const createFetchModule = submodules => {
+  const actions = submodules.map(({ storeName, initialState }) => {
     console.log("dvdfdvedvev", storeName, data)
-    const data = createAction(`FETCH/${storeName}/DATA`)
-    const loading = createAction(`FETCH/${storeName}/LOADING`)
-    const error = createAction(`FETCH/${storeName}/ERROR`)
+    const data = `FETCH/${storeName}/DATA`
+    const loading = `FETCH/${storeName}/LOADING`
+    const error = `FETCH/${storeName}/ERROR`
 
-    // const {
-    //   fetch: {
-    //     user: { data, loading, error },
-    //   },
-    // } = createActions({
-    //   FETCH: {
-    //     [storeName.toUpperCase()]: {
-    //       DATA: data => data,
-    //       LOADING: (isLoading = false) => isLoading,
-    //       ERROR: error => error,
-    //     },
-    //   },
-    // })
-
-    // console.log("{ data, loading, error }", { data, loading, error })
-
-    const initState = initialState || {
-      [storeName]: { loading: false, data: null, error: null },
-    }
-
-    return createFetchReducers({ storeName, initState, data, loading, error })
+    return { data, loading, error, storeName, initialState }
   })
+
+  console.log("actions", actions)
+
+  return createFetchReducers(actions)
+}
 /*
   This creates an opinionated way of handling fetch actions.
   It for user store it creates FETCH/USER/LOADING, FETCH/USER/ERROR and
