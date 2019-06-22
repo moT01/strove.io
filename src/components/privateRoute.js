53e668a435e063adfd5a7c681283d17f89b340bd
adamzaczek <adamzaczekul@gmail.com>
2019-06-22 20:23:45 +0200

    Add private route

---
+++ src/components/privateRoute.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { navigate } from "gatsby"
+import { useDispatch, useSelector } from "react-redux"
+import { createSelector } from "reselect"
+
+const getUserToken = state =>
+  state.fetch.user.data && state.fetch.user.data.siliskyToken
+
+const getToken = createSelector(
+  [getUserToken],
+  token => token
+)
+
+/* Based on https://github.com/gatsbyjs/gatsby/blob/master/examples/simple-auth/src/components/PrivateRoute.js */
+const PrivateRoute = ({ component: Component, location, ...rest }) => {
+  const token = useSelector(getToken)
+
+  console.log("location.pathname", location.pathname)
+
+  if (!token && location.pathname !== `/`) {
+    // If we’re not logged in, redirect to the home page.
+    navigate(`/`)
+    return null
+  }
+
+  return <Component {...rest} />
+}
+
+export default PrivateRoute
