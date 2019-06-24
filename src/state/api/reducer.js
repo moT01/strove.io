412504b6a88b73fa7d6b4a869299ecaeb7281395
adamzaczek <adamzaczekul@gmail.com>
2019-06-24 05:28:08 +0100

    Move api state to separate folder, add api reducer

---
+++ src/state/api/reducer.js
@@ -0,0 +1,46 @@
+import { handleActions } from "redux-actions"
+import * as C from "./constants"
+
+export default handleActions({
+  [C.FETCH_START]: (state, { payload: { name } = {} }) => ({
+    ...state,
+    [name]: {
+      ...(state[name] || {}),
+      isLoading: true,
+      error: undefined,
+      message: undefined,
+      code: undefined,
+    },
+  }),
+  [C.FETCH_SUCCESS]: (
+    state,
+    { payload: { name, data, code, message } = {} }
+  ) => ({
+    ...state,
+    [name]: {
+      ...(state[name] || {}),
+      data: Array.isArray(data)
+        ? [...data]
+        : typeof data === "object"
+        ? { ...state[name].data, ...data }
+        : data,
+      isLoading: false,
+      error: undefined,
+      message,
+      code,
+    },
+  }),
+  [C.FETCH_START]: (
+    state,
+    { payload: { name, error, message, code } = {} }
+  ) => ({
+    ...state,
+    [name]: {
+      ...(state[name] || {}),
+      isLoading: false,
+      error,
+      message,
+      code,
+    },
+  }),
+})
