f0ff6e9ac7dfc1483f2b465567ecd4d1b857bf2e
adamzaczek <adamzaczekul@gmail.com>
2019-06-24 05:52:35 +0100

    Stash changes

--- src/state/index.js
+++ src/state/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux"
 import fetch from "./fetch"
+import api from "./api/reducer"
 
-export default combineReducers({ fetch })
+export default combineReducers({ fetch, api })
