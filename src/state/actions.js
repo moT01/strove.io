7348dc055703a18c92700560cdc6f8199f6c5a10
adamzaczek <adamzaczekul@gmail.com>
2019-06-24 05:15:33 +0100

    Create fetch actions

---
+++ src/state/actions.js
@@ -0,0 +1,18 @@
+import { createAction } from "redux-actions"
+
+import * as C from "./constants"
+
+export const fetchStart = createAction(C.FETCH_START, ({ source, config }) => ({
+  source,
+  config,
+}))
+
+export const fetchSuccess = createAction(
+  C.FETCH_SUCCESS,
+  ({ name, data, code, message }) => ({ data, name, code, message })
+)
+
+export const fetchError = createAction(
+  C.FETCH_ERROR,
+  ({ error, name, code }) => ({ error, name, code })
+)
