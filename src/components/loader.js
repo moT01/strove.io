e892e55c52b959d37065a2d47a26dca5e53836c7
Apple <mateusz@codengo.page>
2019-06-27 11:12:46 +0200

    Some loader work

---
+++ src/components/loader.js
@@ -0,0 +1,67 @@
+// import React from 'react'
+// import styled, { keyframes } from 'styled-components'
+// import { graphql } from 'gatsby'
+// import Img from 'gatsby-image'
+// import { Cog} from '../images/svg'
+
+// const SpinToWin = keyframes`
+//   0% {
+//     transform: rotate(0deg)
+//   }
+//   100% {
+//     transform: rotate(360deg)
+//   }
+// `
+
+// const LoaderWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   height: 93vh;
+//   width: 100vw;
+// `
+
+// const LoaderContainer = styled(LoaderWrapper)`
+//   width: 20vw;
+//   height: auto;
+//   animation: ${SpinToWin} 3s linear infinite;
+// `
+
+// const LogoContainer = styled.div`
+//   width: 8vw;
+//   position: absolute;
+//   z-index: 4;
+// `
+
+// const Loader = props => (
+//   <LoaderWrapper>
+//     {console.log('props', props)}
+//     <LoaderContainer>
+//       <Cog fill="#0072ce" />
+//       <LogoContainer>
+//         {/* <Img fluid={data.imageTwo.childImageSharp.fluid}></Img> */}
+//       </LogoContainer>
+//     </LoaderContainer>
+//   </LoaderWrapper>
+// )
+
+// export default Loader
+
+// export const fluidImage = graphql`
+//   fragment fluidImage on File {
+//     childImageSharp {
+//       fluid(maxWidth: 1000, quality: 80) {
+//         ...GatsbyImageSharpFluid
+//       }
+//     }
+//   }
+// `
+
+// export const pageQuery = graphql`
+//   query {
+//     imageTwo: file(relativePath: { eq: "siliskyLogoHollow.png" }) {
+//       ...fluidImage
+//     }
+//   }
+// `
