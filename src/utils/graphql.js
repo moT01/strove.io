c46f49f2161d60ff3a02c4167e18001757b7b496
adamzaczek <adamzaczekul@gmail.com>
2019-06-30 15:42:04 +0200

    Update docs

--- src/utils/graphql.js
+++ src/utils/graphql.js
@@ -3,19 +3,21 @@ import * as C from 'state/api/constants'
 import defaultClient from '../../client'
 
 /**
- * Gracefully handle mutations
+ * Gracefully handle mutations. Handles all the async aspects of querying, automatically dispatches redux
+ * actions for loading, error and success. Adds ability to handle success and error outside of redux,
+ * for example to set localStorage.
  * @param {Object} objectParam mutation takes one param will all the details, like variables living inside
  * @param {string} objectParam.name - Name of the mutation, for example githubAuth
  * @param {string} objectParam.storeKey - Key where mutation result will be stored unless a custom onSuccessAction or onErrorAction are provided
  * @param {string} objectParam.variables - Mutation variables
  * @param {string} objectParam.context - Mutation context
- * @param {string} objectParam.errorPolicy - Mutation errorPolicy
+ * @param {string} objectParam.errorPolicy - Mutation errorPolicy, defaults to all
  * @param {string} objectParam.mutation - Actual mutation
  * @param {function} objectParam.onSuccess - Function called with the result after mutation succeeds. Example: user => localStorage.setItem('token', user.siliskyToken)
  * @param {function} objectParam.onError - Function called with the error after mutation fails. Example: () => localStorage.removeItem('token')
  * @param {function} objectParam.onSuccessAction - Function called with the result - returns action dispatch. Example: user => ({ type: "USER_LOGIN_SUCCESS", payload: user })
  * @param {function} objectParam.onSuccessError - Function called with the error - returns action dispatch. Example: error => ({ type: "USER_LOGIN_ERROR", payload: error })
- * @param {string} objectParam.dataSelector - Function used to get result data, useful for example for paginated results like data.myProjects.edges
+ * @param {function} objectParam.dataSelector - Function used to get result data, useful for example for paginated results like data.myProjects.edges
  * @param {string} objectParam.client - GraphQL client, for example Github. Defaults to Silisky
  */
 export const mutation = ({
@@ -71,19 +73,22 @@ export const mutation = ({
 }
 
 /**
- * Gracefully handle query
+ * Gracefully handle queries. Handles all the async aspects of querying, automatically dispatches redux
+ * actions for loading, error and success. Adds ability to handle success and error outside of redux,
+ * for example to set localStorage.
  * @param {Object} objectParam mutation takes one param will all the details, like variables living inside
  * @param {string} objectParam.name - Name of the mutation, for example githubAuth
  * @param {string} objectParam.storeKey - Key where mutation result will be stored unless a custom onSuccessAction or onErrorAction are provided
  * @param {string} objectParam.variables - Mutation variables
  * @param {string} objectParam.context - Mutation context
- * @param {string} objectParam.errorPolicy - Mutation errorPolicy
+ * @param {string} objectParam.errorPolicy - Mutation fetchPolicy, defaults to cache-first
+ * @param {string} objectParam.errorPolicy - Mutation errorPolicy, defaults to all
  * @param {string} objectParam.query - Actual query
  * @param {function} objectParam.onSuccess - Function called with the result after query succeeds. Example: user => localStorage.setItem('token', user.siliskyToken)
  * @param {function} objectParam.onError - Function called with the error after query fails. Example: () => localStorage.removeItem('token')
  * @param {function} objectParam.onSuccessAction - Function called with the result - returns action dispatch. Example: user => ({ type: "USER_LOGIN_SUCCESS", payload: user })
  * @param {function} objectParam.onSuccessError - Function called with the error - returns action dispatch. Example: error => ({ type: "USER_LOGIN_ERROR", payload: error })
- * @param {string} objectParam.dataSelector - Function used to get result data, useful for example for paginated results like data.myProjects.edges
+ * @param {function} objectParam.dataSelector - Function used to get result data, useful for example for paginated results like data.myProjects.edges
  * @param {string} objectParam.client - GraphQL client, for example Github. Defaults to Silisky
  */
 export const query = ({
