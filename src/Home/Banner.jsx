4dd9723bf055ccb9daeda04391015fcc7085b4c2
Apple <mateusz@codengo.page>
2019-07-02 20:31:10 +0200

    Homepage inital

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -3,6 +3,13 @@ import QueueAnim from 'rc-queue-anim'
 import TweenOne from 'rc-tween-one'
 import BannerSVGAnim from './component/BannerSVGAnim'
 import styled, { keyframes } from 'styled-components'
+import { useDispatch, useSelector } from 'react-redux'
+import { createSelector } from 'reselect'
+import { selectors } from '../state'
+import Loader from '../components/fullScreenLoader'
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
+const REDIRECT_URI = process.env.GITHUB_REDIRECT_URI
 
 const FadeIn = keyframes`
   0% {
@@ -32,6 +39,34 @@ const Button = styled.button`
   box-shadow: 0 1.5vh 1.5vh -1.5vh #0072ce;
   transition: all 0.2s ease;
   animation: ${FadeIn} 1s ease-out;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
+const LoginButton = styled.a`
+  display: flex;
+  flex-direction: row;
+  height: auto;
+  width: 45%;
+  margin: 5px;
+  padding: 0.5vh;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: ${props => (props.primary ? '#0072ce' : '#ffffff')};
+  border-width: 1px;
+  border-style: solid;
+  color: ${props => (props.primary ? '#ffffff' : '#0072ce')};
+  border-radius: 1vh;
+  border-color: #0072ce;
+  box-shadow: 0 1.5vh 1.5vh -1.5vh #0072ce;
+  transition: all 0.2s ease;
+  animation: ${FadeIn} 1s ease-out;
+  text-decoration: none;
+  cursor: pointer;
 
   &:hover {
     transform: scale(1.1);
@@ -47,7 +82,24 @@ const ButtonsWrapper = styled.div`
   justify-content: space-around;
 `
 
-function Banner(props) {
+const getUserName = selectors.getData('user', null, 'name')
+
+const getUserPhoto = selectors.getData('user', null, 'photoUrl')
+
+const getUserData = createSelector(
+  [getUserName, getUserPhoto],
+  (username, userphoto) => ({ username, userphoto })
+)
+
+const Banner = props => {
+  const isLoading = useSelector(selectors.getLoading('user'))
+  const user = useSelector(getUserData)
+
+  const handleClick = () =>
+    user.username
+      ? console.log('User is logged in')
+      : console.log('User is not logged in')
+
   return (
     <div className="banner-wrapper">
       {props.isMobile && (
@@ -74,11 +126,24 @@ function Banner(props) {
         <h1 key="h1">SiliSky</h1>
         <p key="content">Code in clouds. One evironment for everyone.</p>
         <ButtonsWrapper>
-          <Button primary>
-            <p style={{ margin: 0 }}>Request demo</p>
-          </Button>
+          {isLoading ? (
+            <Button primary>
+              <Loader isFullScreen={false} color={'#ffffff'} height={'2.5vh'} />
+            </Button>
+          ) : user.username ? (
+            <Button primary onClick={handleClick}>
+              <p style={{ margin: 0 }}>Get started</p>
+            </Button>
+          ) : (
+            <LoginButton
+              href={`https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user,user:email,public_repo&redirect_uri=${REDIRECT_URI}`}
+              primary
+            >
+              <p style={{ margin: 0 }}>Get started</p>
+            </LoginButton>
+          )}
           <Button>
-            <p style={{ margin: 0 }}>Learn more</p>
+            <p style={{ margin: 0 }}>Request demo</p>
           </Button>
         </ButtonsWrapper>
       </QueueAnim>
---
+++ src/components/homePage.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import { Location } from '@reach/router'
+import Loader from './fullScreenLoader'
+import SEO from 'components/seo'
+import Layout from 'components/layout'
+
+const FadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`
+
+const PageWrapper = styled(Wrapper)`
+  width: 100vw;
+  height: auto;
+`
+
+const GithubLinkWrapper = styled(Wrapper)`
+  width: 50vw;
+  height: 10vh;
+`
+
+const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  height: auto;
+  width: auto;
+  margin: 5px;
+  padding: 0.5vh;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: ${props => (props.primary ? '#0072ce' : '#ffffff')};
+  border-width: 1px;
+  border-style: solid;
+  color: ${props => (props.primary ? '#ffffff' : '#0072ce')};
+  border-radius: 1vh;
+  border-color: #0072ce;
+  box-shadow: 0 1.5vh 1.5vh -1.5vh #0072ce;
+  transition: all 0.2s ease;
+  animation: ${FadeIn} 1s ease-out;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
+const HomepageComponent = () => {
+  return (
+    <Layout>
+      <SEO title="Preview" />
+      <PageWrapper>
+        <GithubLinkWrapper>
+          <Button primary>Start your project</Button>
+        </GithubLinkWrapper>
+      </PageWrapper>
+    </Layout>
+  )
+}
+
+const Homepage = () => (
+  <Location>
+    {({ location }) => <HomepageComponent location={location} />}
+  </Location>
+)
+
+export default Homepage
