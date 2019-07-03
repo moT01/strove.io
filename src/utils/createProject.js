e774043c26de791924fea057aecc4ad2ed15f5cc
Mateusz <mateusz@codengo.page>
2019-07-03 15:28:40 +0200

    Stash changes

--- src/utils/createProject.js
+++ src/utils/createProject.js
@@ -1,11 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import ApolloClient from 'apollo-boost'
+import { StaticQuery, graphql, navigate } from 'gatsby'
 
 import { query, mutation } from 'utils'
 import * as ApiC from 'state/api/constants'
 import { selectors } from 'state'
+import { ADD_GITHUB_PROJECT, GET_REPO_INFO } from 'queries'
+import * as C from 'state/currentProject/constants'
 
-const addProject = async () => {
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+})
+
+const createProject = async ({ githubLink, dispatch, user }) => {
   const query = GET_REPO_INFO
+
+  const setCurrentProject = ({ editorPort, previewPort, machineId }) => {
+    dispatch({
+      type: C.SELECT_CURRENT_PROJECT,
+      payload: { editorPort, previewPort, machineId },
+    })
+  }
+
+  const startProject = project => {
+    setCurrentProject({
+      editorPort: project.editorPort,
+      previewPort: project.previewPort,
+      machineId: project.machineId,
+    })
+    navigate('/app/editor/', {
+      state: {
+        machineId: project.machineId,
+        editorPort: project.editorPort,
+      },
+    })
+  }
+
   const context = {
     headers: {
       Authorization: `Bearer ${user.githubToken}`,
@@ -47,4 +78,4 @@ const addProject = async () => {
   }
 }
 
-export default addProject
+export default createProject
