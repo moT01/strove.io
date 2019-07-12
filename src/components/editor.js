b264d213d8be0b779c71f8d0525304a28d83c35b
adamzaczek <adamzaczekul@gmail.com>
2019-07-12 20:45:05 +0200

    Comment out project starting in editor for now

--- src/components/editor.js
+++ src/components/editor.js
@@ -3,15 +3,14 @@ import styled from 'styled-components'
 import { useSelector, useDispatch } from 'react-redux'
 import { Location } from '@reach/router'
 import getOr from 'lodash/fp/getOr'
-import { navigate } from 'gatsby'
 
 import Layout from 'components/layout'
 import Loader from 'components/fullScreenLoader.js'
 import { selectors } from 'state'
 import SEO from 'components/seo'
 import * as C from 'state/currentProject/constants'
-import { CONTINUE_PROJECT } from 'queries'
-import { query, mutation } from 'utils'
+// import { CONTINUE_PROJECT } from 'queries'
+// import { query, mutation } from 'utils'
 
 const StyledIframe = styled.iframe`
   display: block;
@@ -35,24 +34,25 @@ const EditorComponent = ({ location }) => {
   const port = useSelector(getPort)
   const [loaderVisible, setLoaderVisible] = useState(true)
 
-  useEffect(() => {
-    // This condition means project has been stopped
-    if (projectId && !machineId) {
-      dispatch(
-        mutation({
-          name: 'continueProject',
-          mutation: CONTINUE_PROJECT,
-          variables: { projectId },
-          onSuccessDispatch: [
-            ({ id, editorPort, previewPort, machineId }) => ({
-              type: C.SELECT_CURRENT_PROJECT,
-              payload: { id, editorPort, previewPort, machineId },
-            }),
-          ],
-        })
-      )
-    }
-  }, [projectId, machineId])
+  // ToDo uncomment and update once API is ready for delayed project stopping
+  // useEffect(() => {
+  //   // This condition means project has been stopped
+  //   if (projectId && !machineId) {
+  //     dispatch(
+  //       mutation({
+  //         name: 'continueProject',
+  //         mutation: CONTINUE_PROJECT,
+  //         variables: { projectId },
+  //         onSuccessDispatch: [
+  //           ({ id, editorPort, previewPort, machineId }) => ({
+  //             type: C.SELECT_CURRENT_PROJECT,
+  //             payload: { id, editorPort, previewPort, machineId },
+  //           }),
+  //         ],
+  //       })
+  //     )
+  //   }
+  // }, [projectId, machineId])
 
   useEffect(() => {
     window.addEventListener('beforeunload', ev => {
