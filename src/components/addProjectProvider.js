84f90da6006bc90eccceb63e6fbfa03a6569bcfc
adamzaczek <adamzaczekul@gmail.com>
2019-07-14 17:15:09 +0200

    Improve code quality

--- src/components/addProjectProvider.js
+++ src/components/addProjectProvider.js
@@ -1,25 +1,15 @@
 import React, { useState, useEffect } from 'react'
-import { ApolloProvider } from 'react-apollo'
-import { Provider, useDispatch, useSelector } from 'react-redux'
-import { createStore as reduxCreateStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
+import { useDispatch, useSelector } from 'react-redux'
 import thunk from 'redux-thunk'
-import { persistStore, persistReducer } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
-import storage from 'redux-persist/lib/storage'
-import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
 import { GITHUB_LOGIN, GITLAB_LOGIN } from 'queries'
-import { mutation, window } from 'utils'
+import { mutation } from 'utils'
 import { createProject } from 'utils'
 import { selectors } from 'state'
 
 import Modal from 'components/modal'
 
 export default ({ children }) => {
-  const [loginWithGithub, setLoginWithGithub] = useState(false)
-  const [loginWithGitlab, setLoginWithGitlab] = useState(false)
-  const [addGithubToLogin, setAddGithubToLogin] = useState(false)
-  const [addGitlabToLogin, setAddGitlabToLogin] = useState(false)
+  const [modalContent, setModalContent] = useState()
 
   const dispatch = useDispatch()
   const user = useSelector(selectors.getUser)
@@ -35,13 +25,13 @@ export default ({ children }) => {
 
     /* ToDo: Handle private github repos */
     if (!user && repoFromGithub) {
-      setLoginWithGithub(true)
+      setModalContent('LoginWithGithub')
     } else if (!user && repoFromGitlab) {
-      setLoginWithGitlab(true)
+      setModalContent('LoginWithGitlab')
     } else if (user && repoFromGithub && !githubToken) {
-      setAddGithubToLogin(true)
+      setModalContent('AddGithubToLogin')
     } else if (user && repoFromGitlab && !gitlabToken) {
-      setAddGitlabToLogin(true)
+      setModalContent('AddGitlabToLogin')
     } else {
       createProject({ repoLink, dispatch, user })
     }
@@ -51,12 +41,12 @@ export default ({ children }) => {
     <>
       {children({ addProject })}
       <Modal
-        isOpen={loginWithGithub}
-        onRequestClose={() => setLoginWithGithub(false)}
-        contentLabel="Login first"
+        isOpen={!!modalContent}
+        onRequestClose={() => setModalContent()}
+        contentLabel={modalContent}
         ariaHideApp={false}
       >
-        Login with github
+        {modalContent}
       </Modal>
     </>
   )
