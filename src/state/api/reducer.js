2caacb78cd5a0010bf35fb779f4526f70dfca418
adamzaczek <adamzaczekul@gmail.com>
2019-07-16 20:50:41 +0200

    Make api reducer even smarter

--- src/state/api/reducer.js
+++ src/state/api/reducer.js
@@ -39,14 +39,21 @@ export default handleActions(
     ) => {
       let newData
 
-      if (
-        Array.isArray(state[storeKey].data) &&
-        Array.isArray(state[storeKey].data)
-      ) {
-        newData = [...state[storeKey].data, ...data]
-      } else if (Array.isArray(state[storeKey].data)) {
+      /*
+        Default, opinionated behavior for reducers
+        If old data was an array and one one isn't, append new data to array (for example
+        when adding additional project to projects)
+        If old wata was an object extend object with new props (for example when user is already
+        logged in but more user data has beeen fetched)
+        For old data being an array or primitive and new data coming in array or primitive overwrite
+        old data with a new one (for example when re-fetching projects
+      */
+      if (Array.isArray(state[storeKey].data) && !Array.isArray(data)) {
         newData = [...state[storeKey].data, data]
-      } else if (typeof state[storeKey].data === 'object') {
+      } else if (
+        !Array.isArray(state[storeKey].data) &&
+        typeof state[storeKey].data === 'object'
+      ) {
         newData = { ...state[storeKey].data, ...data }
       } else {
         newData = data
