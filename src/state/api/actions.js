4fa32653381fb62f4b8133c9d336f3da5eb8aeda
adamzaczek <adamzaczekul@gmail.com>
2019-07-23 19:22:53 +0200

    Update api actions

--- src/state/api/actions.js
+++ src/state/api/actions.js
@@ -1,18 +1,22 @@
 import * as C from './constants'
 
-export const fetchStart = ({ source, config }) => ({
+export const fetchStart = ({ storeKey, source, config }) => ({
   type: C.FETCH_START,
-  payload: { source, config },
+  payload: { storeKey, source, config },
 })
 
-export const fetchSuccess = ({ name, data, code, message }) => ({
-  type: C.FETCH_SUCCESS,
-  payload: { data, name, code, message },
-})
+export const fetchSuccess = ({ storeKey, data, code, message }) =>
+  console.log({ storeKey, data, code, message }) || {
+    type: C.FETCH_SUCCESS,
+    payload: { data, storeKey, code, message },
+  }
 
-export const fetchError = ({ error, name, code }) => ({
+export const fetchError = ({ storeKey, error, code }) => ({
   type: C.FETCH_ERROR,
-  payload: { error, name, code },
+  payload: { error, storeKey, code },
 })
 
-export const removeItem = ({ id }) => ({ type: C.REMOVE_ITEM, payload: { id } })
+export const removeItem = ({ id, storeKey }) => ({
+  type: C.REMOVE_ITEM,
+  payload: { id, storeKey },
+})
