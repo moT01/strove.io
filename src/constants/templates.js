3df6456bcfa5e1d9f466fa8647ebdff21508884d
adamzaczek <adamzaczekul@gmail.com>
2019-08-05 02:21:10 +0200

    Move templates to constants

---
+++ src/constants/templates.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import {
+  CSharp,
+  Cpp,
+  Python,
+  Typescript,
+  Go,
+  Java,
+  Ruby,
+  Javascript,
+} from 'images/logos'
+
+export default [
+  {
+    name: 'Typescript',
+    icon: <Typescript />,
+    link: 'https://github.com/codengo-llc/TypeScript-HelloWorld',
+  },
+  {
+    name: 'Ruby',
+    icon: <Ruby />,
+    link: 'https://github.com/codengo-llc/ruby-starter',
+  },
+  {
+    name: 'Javascript',
+    icon: <Javascript />,
+    link: 'https://github.com/codengo-llc/gatsby-starter',
+  },
+  {
+    name: 'C++',
+    icon: <Cpp />,
+    link: 'https://github.com/codengo-llc/c-plus-plus-starter',
+  },
+  {
+    name: 'Python',
+    icon: <Python />,
+    link: 'https://github.com/codengo-llc/python-starter',
+  },
+  {
+    name: 'Go',
+    icon: <Go />,
+    link: 'https://github.com/codengo-llc/Go-HelloWorld',
+  },
+  {
+    name: 'C#',
+    icon: <CSharp />,
+    link: 'https://github.com/codengo-llc/C-Sharp-starter',
+  },
+  {
+    name: 'Java',
+    icon: <Java />,
+    link: 'https://github.com/codengo-llc/java-starter',
+  },
+]
