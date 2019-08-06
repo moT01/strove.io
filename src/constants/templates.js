b8e39e6fb8d251031e56a41eb8a9a3071445f1e3
adamzaczek <adamzaczekul@gmail.com>
2019-08-06 09:53:14 +0200

    Use new repo addresses

--- src/constants/templates.js
+++ src/constants/templates.js
@@ -15,41 +15,41 @@ export default [
   {
     name: 'Typescript',
     icon: <Typescript />,
-    link: 'https://github.com/codengo-llc/TypeScript-HelloWorld',
+    link: 'https://github.com/silisky/TypeScript-HelloWorld',
   },
   {
     name: 'Ruby',
     icon: <Ruby />,
-    link: 'https://github.com/codengo-llc/ruby-starter',
+    link: 'https://github.com/silisky/ruby-starter',
   },
   {
     name: 'Javascript',
     icon: <Javascript />,
-    link: 'https://github.com/codengo-llc/gatsby-starter',
+    link: 'https://github.com/silisky/gatsby-starter',
   },
   {
     name: 'C++',
     icon: <Cpp />,
-    link: 'https://github.com/codengo-llc/c-plus-plus-starter',
+    link: 'https://github.com/silisky/c-plus-plus-starter',
   },
   {
     name: 'Python',
     icon: <Python />,
-    link: 'https://github.com/codengo-llc/python-starter',
+    link: 'https://github.com/silisky/python-starter',
   },
   {
     name: 'Go',
     icon: <Go />,
-    link: 'https://github.com/codengo-llc/Go-HelloWorld',
+    link: 'https://github.com/silisky/Go-HelloWorld',
   },
   {
     name: 'C#',
     icon: <CSharp />,
-    link: 'https://github.com/codengo-llc/C-Sharp-starter',
+    link: 'https://github.com/silisky/C-Sharp-starter',
   },
   {
     name: 'Java',
     icon: <Java />,
-    link: 'https://github.com/codengo-llc/java-starter',
+    link: 'https://github.com/silisky/java-starter',
   },
 ]
