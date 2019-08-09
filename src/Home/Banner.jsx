f9b3c7844db2d8598e9a588a6d85a750dafa9718
Apple <mateusz@codengo.page>
2019-08-09 19:30:03 +0200

    Hopefully fixed modal offset

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -95,11 +95,11 @@ const StyledModal = styled(Modal)`
   height: auto;
   width: auto;
   top: 25%;
-  ${({ isMobile }) =>
+  /* ${({ isMobile }) =>
     !isMobile &&
     css`
       left: 25%;
-    `}
+    `} */
   position: fixed;
   animation: ${FadeIn} 0.2s ease-out;
 
