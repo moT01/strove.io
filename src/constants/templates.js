b3c780a59d6e38357d2580f0ec3ba5ac9376f1ad
adamzaczek <adamzaczekul@gmail.com>
2019-08-11 22:16:03 +0200

    Update readme links

--- src/constants/templates.js
+++ src/constants/templates.js
@@ -15,41 +15,41 @@ export default [
   {
     name: 'Typescript',
     icon: <Typescript />,
-    link: 'https://github.com/ficdevelopers/TypeScript-HelloWorld',
+    link: 'https://github.com/stroveio/TypeScript-HelloWorld',
   },
   {
     name: 'Ruby',
     icon: <Ruby />,
-    link: 'https://github.com/ficdevelopers/ruby-starter',
+    link: 'https://github.com/stroveio/ruby-starter',
   },
   {
     name: 'Javascript',
     icon: <Javascript />,
-    link: 'https://github.com/ficdevelopers/gatsby-starter',
+    link: 'https://github.com/stroveio/gatsby-starter',
   },
   {
     name: 'C++',
     icon: <Cpp />,
-    link: 'https://github.com/ficdevelopers/c-plus-plus-starter',
+    link: 'https://github.com/stroveio/c-plus-plus-starter',
   },
   {
     name: 'Python',
     icon: <Python />,
-    link: 'https://github.com/ficdevelopers/python-starter',
+    link: 'https://github.com/stroveio/python-starter',
   },
   {
     name: 'Go',
     icon: <Go />,
-    link: 'https://github.com/ficdevelopers/Go-HelloWorld',
+    link: 'https://github.com/stroveio/Go-HelloWorld',
   },
   {
     name: 'C#',
     icon: <CSharp />,
-    link: 'https://github.com/ficdevelopers/C-Sharp-starter',
+    link: 'https://github.com/stroveio/C-Sharp-starter',
   },
   {
     name: 'Java',
     icon: <Java />,
-    link: 'https://github.com/ficdevelopers/java-starter',
+    link: 'https://github.com/stroveio/java-starter',
   },
 ]
