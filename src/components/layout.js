9d4a85ca29266159745a9f535d82b3fae048847e
Apple <mateusz@codengo.page>
2019-08-13 19:17:33 +0200

    Activity indicator is even better. Also added info about supported browsers

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React, { memo } from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
+import DetectBrowser from 'react-detect-browser'
 
 import Header from './header'
 import './layout.css'
@@ -31,7 +32,22 @@ const Layout = ({ children }) => {
       render={data => (
         <>
           <Header siteTitle={data.site.siteMetadata.title} />
-          <MainContent>{children}</MainContent>
+          <MainContent>
+          
+          <DetectBrowser>
+            {({ browser }) =>
+              (browser && browser.name === 'chrome') ||
+              (browser && browser.name === 'firefox') ? (
+                <div></div>
+              ) : (
+                <div>
+                  Your browser is not supported. Strove.io currenty supports
+                  only Google Chrome nad Mozilla Firefox
+                </div>
+              )
+            }
+          </DetectBrowser>
+          {children}</MainContent>
         </>
       )}
     />
