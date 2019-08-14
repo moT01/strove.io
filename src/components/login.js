7682131ce7aa7c75254ad9401fed5e302a1122cd
adamzaczek <adamzaczekul@gmail.com>
2019-08-14 21:43:53 +0200

    Make login behave somewhat better

--- src/components/login.js
+++ src/components/login.js
@@ -99,7 +99,7 @@ const MenuWrapper = styled.div`
   background-color: ${props => (props.invert ? '#ffffff' : '#0072ce')};
   z-index: 3;
   left: -2vw;
-  position: absolute;
+  position: relative;
   @media (max-width: 1365px) {
     left: -6vw;
   }
@@ -113,6 +113,8 @@ const Option = styled.a`
   margin: ${props => (props.isLast ? `0` : `0 0 0.2vh`)};
   width: auto;
   height: auto;
+  /* position: absolute;
+  top: 25px; */
   font-size: 1.2rem;
   color: ${props => (!props.invert ? '#ffffff' : '#0072ce')};
   border-bottom-left-radius: ${props => props.isLast && '5px'};
