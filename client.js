c31feaa5b3ad5bdf0def153398ee2bef90da8ed8
adamzaczek <adamzaczekul@gmail.com>
2019-08-15 17:28:33 +0200

    Fix client

--- client.js
+++ client.js
@@ -1,11 +1,26 @@
 import { ApolloClient } from 'apollo-client'
+import { split } from 'apollo-link'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { onError } from 'apollo-link-error'
+import { withClientState } from 'apollo-link-state'
+import { ApolloLink, Observable } from 'apollo-link'
+import fetch from 'isomorphic-fetch'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
-import { split } from 'apollo-link'
 
 const cache = new InMemoryCache()
+
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+}
 
 const wsLink = process.browser
   ? new WebSocketLink({
@@ -34,7 +49,68 @@ const link = process.browser
     )
   : httpLink
 
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable(observer => {
+      let handle
+      Promise.resolve(operation)
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer),
+          })
+        })
+        .catch(observer.error.bind(observer))
+
+      return () => {
+        if (handle) handle.unsubscribe()
+      }
+    })
+)
+
 export default new ApolloClient({
-  link,
+  fetch,
+  defaultOptions,
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        // sendToLoggingService(graphQLErrors)
+        graphQLErrors.map(({ message, locations, path, extensions }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        )
+        if (
+          graphQLErrors.find(
+            ({ extensions }) => extensions.code === 'FORBIDDEN'
+          )
+        ) {
+          // logout()
+          console.log('FORBIDDEN!')
+        }
+      }
+      if (networkError) {
+        // logoutUser()
+        console.log(`[Network error]: ${networkError}`)
+      }
+    }),
+    requestLink,
+    withClientState({
+      defaults: {
+        isConnected: true,
+      },
+      resolvers: {
+        Mutation: {
+          updateNetworkStatus: (_, { isConnected }, { cache }) => {
+            cache.writeData({ data: { isConnected } })
+            return null
+          },
+        },
+      },
+      cache,
+    }),
+    link,
+  ]),
   cache,
 })
