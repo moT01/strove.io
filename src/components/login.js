2f143af6b001a33e144f13d226ea0ac507aaa536
adamzaczek <adamzaczekul@gmail.com>
2019-08-16 05:34:53 +0200

    Improve login position

--- src/components/login.js
+++ src/components/login.js
@@ -86,6 +86,7 @@ const MenuWrapper = styled.div`
   border-style: solid;
   background-color: ${props => (props.invert ? '#ffffff' : '#0072ce')};
   z-index: 3;
+  left: ${props => (props.login ? '-2vw' : '30px')};
   position: relative;
 
   @media (max-width: 1365px) {
@@ -198,7 +199,7 @@ const LoginDropdown = () => {
             hidden={!isOpen}
             style={{ position: 'absolute', background: 'none' }}
           >
-            <MenuWrapper invert>
+            <MenuWrapper invert login>
               {loginOptions.map((item, index) => (
                 <Option
                   invert
