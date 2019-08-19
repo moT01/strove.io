f471eb2bb59966a3a515dab4bead23826875ff1d
adamzaczek <adamzaczekul@gmail.com>
2019-08-19 18:35:32 +0200

    Remove redundant import

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,4 @@
-import React, { useState, memo } from 'react'
+import React, { memo } from 'react'
 import styled from 'styled-components'
 import { createSelector } from 'reselect'
 import Downshift from 'downshift'
