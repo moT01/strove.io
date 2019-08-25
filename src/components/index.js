7ecacb6b50917eab6a8bd0e2c6fcccc239472b72
adamzaczek <adamzaczekul@gmail.com>
2019-08-25 02:27:42 +0200

    Export all components from index

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,35 @@ import Header from './header'
 import Image from './image'
 import Login from './logos'
 import Seo from './seo'
+import AddProjectModals from './addProjectModals'
+import AddProjectProvider from './addProjectProvider'
+import FullScreenLoader from './fullScreenLoader'
+import Dashboard from './dashboard'
+import Editor from './editor'
+import GetStarted from './getStarted'
+import Layout from './layout'
+import Logos from './logos.js'
+import Modal from './modal'
+import Preview from './preview'
+import PrivateRoute from './privateRoute'
 
-export { SmallCloud, MediumCloud, BigCloud, Header, Image, Login, Seo }
+export {
+  SmallCloud,
+  MediumCloud,
+  BigCloud,
+  Header,
+  Image,
+  Login,
+  Seo,
+  AddProjectModals,
+  AddProjectProvider,
+  FullScreenLoader,
+  Dashboard,
+  Editor,
+  GetStarted,
+  Layout,
+  Logos,
+  Modal,
+  Preview,
+  PrivateRoute,
+}
