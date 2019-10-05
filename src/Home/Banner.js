50ff0445a80958d150ff04b38e9d2261b254eb5b
Mateusz Olek <mateusz@codengo.page>
2019-10-05 15:31:40 +0200

    Switched video source from yt to html

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -97,10 +97,9 @@ const ButtonsWrapper = styled.div`
   justify-content: space-around;
 `
 
-const Video = styled.iframe`
+const Video = styled.video`
   height: ${props => (props.isMobile ? '67vw' : '26vw')};
   width: ${props => (props.isMobile ? '90vw' : '35vw')};
-  border: none;
   margin-top: ${props => (props.isMobile ? '5vh' : '0')};
 `
 
@@ -134,6 +133,32 @@ const StyledIcon = styled(Icon)`
   :focus {
     outline: none;
   }
+`
+
+const TextWell = styled.div`
+  color: black;
+  height: auto;
+  width: ${isMobile ? '100vw' : '40vw'};
+  margin: 0vw 7.5vw 0 7.5vw;
+  padding: 3vh;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+  text-align: left;
+  text-justify: inter-word;
+  background-color: white;
+`
+
+const Paragraph = styled.p`
+  text-indent: 30px;
+  font-size: 16px;
+  display: inline-block;
+  padding-top: 0;
+`
+
+const Header = styled.h3`
+  font-size: 22;
+  display: inline-block;
 `
 
 const Banner = () => {
@@ -190,9 +215,29 @@ const Banner = () => {
           </SectionWrapper>
           <SectionWrapper>
             <Video
-              src="https://www.youtube.com/embed/wbNlI3rH_Go"
               isMobile={isMobile}
+              src="../videos/StroveDemo.mp4"
+              controls
             ></Video>
+            <TextWell>
+              <Header>Why Strove?</Header>
+              <Paragraph>
+                Strove.io solves 3 problems programmers face: 1. Programmers
+                waste a lot of time on setting up the environment - this problem
+                is very troublesome for many teams and has been our initial
+                inspiration to start working on this project. Strove.io can cut
+                the time to set a programming environment to seconds. 2. It is
+                hard to use different computers when working on a project and
+                code breaks across different devices and operating systems. In
+                Strove.io the code lives in a cloud. This makes it possible to
+                resume working on a project from any computer with internet
+                access. 3. 'It works on my machine' problem. Strove.io lets
+                programmers use their favorite devices without sacrificing
+                productivity. All team members access instances of the same
+                programming environment, no matter if they use Macbook or an old
+                PC.
+              </Paragraph>
+            </TextWell>
           </SectionWrapper>
         </SectionDivider>
       </div>
