e07391dd0318366f3b8a53d16897de2d5ad91b53
adamzaczek <adamzaczekul@gmail.com>
2019-10-11 17:45:19 +0200

    Fix logos margin

--- src/components/logos.js
+++ src/components/logos.js
@@ -55,7 +55,7 @@ const IconContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   cursor: pointer;
-  margin: 0 50px 0;
+  margin: 0 50px 0 0;
   border-radius: 8px;
   opacity: 0.9;
 
