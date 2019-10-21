6cb3ca9e9bfd593f9d1f2a54c9b9ac1b9b0180b1
adamzaczek <adamzaczekul@gmail.com>
2019-10-22 00:57:23 +0200

    Remove redundant condition

--- src/components/addProjectModals.js
+++ src/components/addProjectModals.js
@@ -275,7 +275,6 @@ const AddProjectModals = ({
         closeModal={closeModal}
         isOpen={
           modalContent === 'AddEmptyProject' ||
-          modalContent === 'PrivateRepo' ||
           modalContent === 'UnableToClone'
         }
         modalContent={modalContent}
