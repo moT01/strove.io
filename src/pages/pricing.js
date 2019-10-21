4ae63c0e066c1bf4e7c5efd03b81f56b640e30f6
AdamZaczek <adamzaczekul@gmail.com>
2019-10-21 18:45:44 +0200

    Re-add missing component

---
+++ src/pages/pricing.js
@@ -0,0 +1,526 @@
+import React, { useState, memo } from 'react'
+import styled, { keyframes, css } from 'styled-components'
+import StripeCheckout from 'react-stripe-checkout'
+import { isMobileOnly, isTablet } from 'react-device-detect'
+import { useSelector, useDispatch } from 'react-redux'
+import { Formik, Form, Field } from 'formik'
+import isEmail from 'validator/lib/isEmail'
+
+import { BUY_SUBSCRIPTION, SEND_EMAIL } from 'queries'
+import { mutation } from 'utils'
+import SEO from 'components/seo'
+import Layout from 'components/layout'
+import { C } from 'state'
+import Modal from 'components/modal'
+import { selectors } from 'state'
+
+const ButtonFadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
+
+const StyledTrialInfo = styled.ul`
+  font-size: 13px;
+  padding: 0;
+  margin: 0;
+  color: ${({ team }) => (team ? '#fff' : '#0072ce')};
+
+  li {
+    display: inline-block;
+    margin-right: 8px;
+    list-style: none;
+
+    &:before {
+      margin-right: 0.3em;
+      content: '✔';
+      color: ${({ team }) => (team ? '#fff' : '#0072ce')};
+    }
+  }
+`
+
+const CardsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 3vh;
+  animation: ${ButtonFadeIn} 0.3s ease-out;
+
+  @media (max-width: 1366px) {
+    flex-direction: column;
+  }
+`
+
+const StyledH6 = styled.h6`
+  margin: 20px;
+  color: #0072ce;
+`
+
+const EmailFormWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  min-width: 400px;
+
+  flex-wrap: wrap;
+  margin: 10px 0 5px;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(174, 174, 186, 0.24),
+    0 8px 24px 0 rgba(174, 174, 186, 0.16);
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  border-radius: 5px;
+  transition: all 0.2s ease;
+  opacity: 0.9;
+  align-items: center;
+  background: #0072ce;
+
+  ${({ isMobile }) =>
+    isMobile &&
+    css`
+      flex-direction: column;
+      box-shadow: none;
+      min-width: 100px;
+      border-radius: 5px;
+    `}
+
+  &:hover {
+    opacity: 1;
+    box-shadow: 0 3px 5px 0 rgba(174, 174, 186, 0.24),
+      0 9px 26px 0 rgba(174, 174, 186, 0.16);
+
+    ${({ isMobile }) =>
+      isMobile &&
+      css`
+        box-shadow: none;
+      `}
+  }
+
+  input {
+    box-shadow: none;
+    color: #333e63;
+    outline: 0;
+    background: #fff;
+    width: calc(100% - 156px);
+    height: 56px;
+    padding: 0;
+    padding-left: 64px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    line-height: 36px;
+    font-size: 17px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    letter-spacing: 0.2px;
+    border: 0;
+    border-radius: 5px;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+
+    ${({ isMobile }) =>
+      isMobile &&
+      css`
+        flex-direction: column;
+        box-shadow: 0 2px 4px 0 rgba(174, 174, 186, 0.24),
+          0 8px 24px 0 rgba(174, 174, 186, 0.16);
+        border-radius: 5px;
+        width: 100%;
+      `}
+  }
+
+  svg {
+    position: absolute;
+    top: 18px;
+    left: 20px;
+    height: 24px;
+    width: 24px;
+
+    g {
+      stroke: #0072ce;
+    }
+  }
+
+  button {
+    width: 156px;
+    height: 56px;
+    color: #fff;
+    background: #000;
+    text-transform: uppercase;
+    display: block;
+    text-align: center;
+    padding: 0;
+    border: 0;
+    font-size: 14px;
+    font-weight: bold;
+    line-height: normal;
+    letter-spacing: 0.8px;
+    transition: opacity 0.2s;
+    border-radius: 5px;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    outline: none;
+
+    ${({ isMobile }) =>
+      isMobile &&
+      css`
+        box-shadow: 0 2px 4px 0 rgba(174, 174, 186, 0.24),
+          0 8px 24px 0 rgba(174, 174, 186, 0.16);
+        border-radius: 5px;
+        width: 100%;
+        margin-top: 10px;
+      `}
+
+    ${props =>
+      !props.disabled
+        ? css`
+            cursor: pointer;
+            &:hover {
+              opacity: 1;
+              box-shadow: 0 3px 5px 0 rgba(174, 174, 186, 0.24),
+                0 9px 26px 0 rgba(174, 174, 186, 0.16);
+            }
+          `
+        : css`
+            cursor: not-allowed;
+          `}
+  }
+`
+
+const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  height: auto;
+  width: 75%;
+  margin: 15px 0 5px;
+  padding: 0.5vh;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: ${props => (props.team ? '#ffffff' : '#0072ce')};
+  border-width: 1px;
+  border-style: solid;
+  color: ${props => (!props.team ? '#ffffff' : '#0072ce')};
+  border-radius: 4px;
+  border-color: ${props => (props.team ? '#ffffff' : '#0072ce')};
+  box-shadow: 0 1.1vh 1.1vh -1.5vh ${props => (props.team ? '#ffffff' : '#0072ce')};
+  transition: all 0.2s ease;
+
+  &:disabled {
+    opacity: 0.4;
+  }
+
+  ${props =>
+    !props.disabled &&
+    css`
+      animation: ${ButtonFadeIn} 1s ease-out;
+      cursor: pointer;
+      &:hover {
+        opacity: 1;
+        box-shadow: 0 1.3vh 1.3vh -1.3vh ${props => (props.team ? '#ffffff' : '#0072ce')};
+        transform: translateY(-3px);
+      }
+    `}
+
+  @media (max-width: 1366px) {
+    box-shadow: 0 1.2vh 1.2vh -1.5vh ${props => (props.team ? '#ffffff' : '#0072ce')};
+  }
+`
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => (props.team ? '#0072ce' : '#ffffff')};
+  margin-left: ${props => (props.team ? '-3vw' : 0)};
+  margin-top: ${props => (props.team ? '9vh' : 0)};
+  z-index: auto;
+  border-radius: 5px;
+  border-color: #0072ce;
+  border-width: 1px;
+  border-style: solid;
+  padding: 50px 20px;
+  box-shadow: 0 1.5vh 1.5vh -1.5vh #0072ce;
+
+  width: 40vw;
+  @media (max-width: 1366px) {
+    width: 80vw;
+    height: auto;
+  }
+`
+
+const PricingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  height: 80%;
+
+  @media (max-width: 1366px) {
+    width: 90%;
+    height: auto;
+  }
+`
+
+const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`
+
+const Text = styled.p`
+  color: #0072ce;
+  font-size: 15px;
+  margin-left: 2%;
+  margin-bottom: 12px;
+  white-space: normal;
+  text-overflow: wrap;
+  overflow: visible;
+  text-align: center;
+`
+const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: ${props =>
+    props.mobile === 'mobile'
+      ? '85%'
+      : props.mobile === 'tablet'
+      ? '60%'
+      : '45%'};
+`
+
+const PricingSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+  @media (max-width: 1366px) {
+    flex-direction: column;
+  }
+
+  span {
+    width: 75%;
+  }
+`
+
+const Feature = styled.p`
+  color: ${props => (!props.team ? '#0072ce' : '#ffffff')};
+  font-size: 15px;
+  margin-top: 0.7vh;
+  margin-bottom: 0.7vh;
+`
+const PlanTitle = styled.h1`
+  margin: 10px;
+  font-size: 26px;
+  color: ${props => (!props.team ? '#0072ce' : '#ffffff')};
+  font-weight: 500;
+`
+const Contact = styled(PlanTitle)`
+  width: 75%;
+`
+const Price = styled(PlanTitle)`
+  font-size: 30px;
+`
+const PlanSubTitle = styled(PlanTitle)`
+  font-size: 20px;
+`
+const ButtonText = styled(PlanTitle)`
+  font-size: 16px;
+`
+const PlanDesc = styled(Feature)`
+  font-size: 15px;
+`
+const CardTitle = styled(PlanTitle)`
+  font-size: 40px;
+`
+
+const StripeButton = styled(Button)`
+  width: 100%;
+`
+
+const validate = values => {
+  let errors = {}
+
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!isEmail(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  return errors
+}
+
+const PricingPage = () => {
+  const [emailSent, setEmailSent] = useState(false)
+  const dispatch = useDispatch()
+  const [modalVisible, setModalVisible] = useState(false)
+  const subscription = useSelector(selectors.api.getApiData('subscription'))
+
+  const device = isMobileOnly ? 'mobile' : isTablet ? 'tablet' : 'computer'
+  const queryToken = ({ id }) => {
+    if (id) {
+      dispatch(
+        mutation({
+          name: 'buySubscription',
+          storeKey: 'subscription',
+          mutation: BUY_SUBSCRIPTION,
+          variables: { tokenId: id },
+          onSuccess: data => setModalVisible(true),
+          onSuccessDispatch: [
+            buySubscription => ({
+              type: C.api.UPDATE_ITEM,
+              payload: { storeKey: 'subscription', data: buySubscription },
+            }),
+          ],
+        })
+      )
+    }
+  }
+
+  return (
+    <>
+      <Modal
+        isOpen={!!modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+        contentLabel={'Activate Pro plan'}
+        ariaHideApp={false}
+        width={isMobileOnly ? '70vw' : isTablet ? '50vw' : '30vw'}
+        height={isMobileOnly ? '40vh' : '25vh'}
+        zIndex={99}
+      >
+        <ModalWrapper>
+          <CardTitle>Congratulations!</CardTitle>
+          <Text>Your purchase has been successfully completed.</Text>
+          <ButtonsWrapper mobile={device}>
+            <Button onClick={() => setModalVisible(false)}>Close</Button>
+          </ButtonsWrapper>
+        </ModalWrapper>
+      </Modal>
+      <Layout>
+        <SEO title="Pricing" />
+        <CardsWrapper>
+          <Card>
+            <CardTitle>Individuals</CardTitle>
+            <PricingSection>
+              <PricingWrapper>
+                <PlanTitle>Free</PlanTitle>
+                <PlanDesc>The basic plan for individual developers</PlanDesc>
+                <Price>$0</Price>
+                <PlanSubTitle>Per month</PlanSubTitle>
+                <Feature>Public repositories</Feature>
+                <Feature>6 GB of RAM</Feature>
+                <Feature>No commercial use</Feature>
+                <Button disabled>
+                  <ButtonText team>Already subscribed</ButtonText>
+                </Button>
+                <StyledTrialInfo>
+                  <li>Free for non-commercial use</li>
+                </StyledTrialInfo>
+              </PricingWrapper>
+            </PricingSection>
+          </Card>
+          <Card team>
+            <CardTitle team>Teams</CardTitle>
+            <PricingSection>
+              <PricingWrapper>
+                <PlanTitle team>Enterprise</PlanTitle>
+                <PlanDesc team>
+                  Plan aimed for developer teams and companies
+                </PlanDesc>
+                <Contact team>Contact Sales for pricing</Contact>
+                <Feature team>Private and Public repositories</Feature>
+                <Feature team>
+                  RAM, hard drive and speed adjusted to team's needs
+                </Feature>
+                <Feature team>Commercial use</Feature>
+                <Feature team>Priority support</Feature>
+                <Formik
+                  initialValues={{
+                    email: '',
+                  }}
+                  validate={validate}
+                  onSubmit={values => {
+                    dispatch(
+                      mutation({
+                        name: 'sendEmail',
+                        context: null,
+                        mutation: SEND_EMAIL,
+                        variables: { email: values.email, isDemo: true },
+                        onSuccess: () => setEmailSent(true),
+                      })
+                    )
+                  }}
+                >
+                  {({ errors, touched, values }) => (
+                    <Form>
+                      <EmailFormWrapper
+                        disabled={errors.email || !values.email}
+                        isMobile={isMobileOnly}
+                      >
+                        <Field
+                          type="email"
+                          name="email"
+                          placeholder="Your Email"
+                        ></Field>
+                        <svg
+                          className="Form-fieldGroupIcon"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                        >
+                          <g
+                            fill="none"
+                            fill-rule="evenodd"
+                            stroke="#9CA2B4"
+                            stroke-width="2"
+                          >
+                            <path d="M2 4h20v16H2z"></path>
+                            <path d="M2 7.9l9.9 3.899 9.899-3.9"></path>
+                          </g>
+                        </svg>
+                        <button
+                          type="submit"
+                          isDisabled={
+                            values.email && errors.email && touched.email
+                          }
+                        >
+                          Request demo
+                        </button>
+                      </EmailFormWrapper>
+                      <StyledTrialInfo team>
+                        <li>Free 14-day Demo</li>
+                        <li>No credit card needed</li>
+                        <li>No setup</li>
+                      </StyledTrialInfo>
+                      {emailSent && (
+                        <StyledH6>Thank you, we'll get in touch soon!</StyledH6>
+                      )}
+                    </Form>
+                  )}
+                </Formik>
+              </PricingWrapper>
+            </PricingSection>
+          </Card>
+        </CardsWrapper>
+      </Layout>
+    </>
+  )
+}
+
+export default memo(PricingPage)
