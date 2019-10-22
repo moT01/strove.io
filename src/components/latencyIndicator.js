df50fe46f59aa061fb7397f6b388e8da6ee22b42
adamzaczek <adamzaczekul@gmail.com>
2019-10-23 01:59:45 +0200

    Add latency unit

--- src/components/latencyIndicator.js
+++ src/components/latencyIndicator.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, memo } from 'react'
 import styled from 'styled-components'
-import LatencyMonitor from 'latency-monitor';
+import LatencyMonitor from 'latency-monitor'
 
 const LatencyCircle = styled.div`
   border-radius: 50%;
@@ -11,7 +11,12 @@ const LatencyCircle = styled.div`
   background: #fff;
   color: #666;
   text-align: center;
-  background-color: ${({ latency }) => latency > 10 && latency < 30 ? 'yellow' : latency <= 10 ? '#39e626' : '#ef7a2c'};
+  background-color: ${({ latency }) =>
+    latency > 10 && latency < 30
+      ? 'yellow'
+      : latency <= 10
+      ? '#39e626'
+      : '#ef7a2c'};
 `
 
 const LatencyWrapper = styled.div`
@@ -27,14 +32,14 @@ const LatencyIndicator = () => {
   const [latency, setLatency] = useState(20)
 
   useEffect(() => {
-    const monitor = new LatencyMonitor();
-    monitor.on('data', ({ avgMs }) => setLatency(avgMs));
+    const monitor = new LatencyMonitor()
+    monitor.on('data', ({ avgMs }) => setLatency(avgMs))
   }, [])
 
   return (
     <LatencyWrapper>
       <LatencyCircle latency={latency} />
-      <StyledText>Latency: {Math.round(latency)}</StyledText>
+      <StyledText>Latency: {Math.round(latency)}ms</StyledText>
     </LatencyWrapper>
   )
 }
