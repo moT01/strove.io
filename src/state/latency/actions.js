df6ceae94e84931f33616b95550ffb6dad963e47
adamzaczek <adamzaczekul@gmail.com>
2019-10-25 14:35:02 +0200

    Update names

--- src/state/latency/actions.js
+++ src/state/latency/actions.js
@@ -1,11 +1,11 @@
 import * as C from './constants'
 
-export const addIncomingProject = payload => ({
+export const latencyMeasureStart = payload => ({
   type: C.LATENCY_MEASURE_START,
   payload,
 })
 
-export const catchIncomingError = payload => ({
+export const LatencyMeasureEnd = payload => ({
   type: C.LATENCY_MEASURE_END,
   payload,
 })
