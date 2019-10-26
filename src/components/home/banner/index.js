ea09b9ce60efa1a327b7d1d867f29dacebb40d52
adamzaczek <adamzaczekul@gmail.com>
2019-10-26 20:53:49 +0200

    Add dynamic import module

--- src/components/home/banner/index.js
+++ src/components/home/banner/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import { window } from 'utils'
+import { useInterval } from 'hooks'
 
 import Education from './education'
 import Enterprise from './enterprise'
 
+const checkGoogleOptimizeLoading = () => {
+  if (window?.google_optimize !== undefined) {
+    const variant = window.google_optimize.get(process.env.GOOGLE_OPTIMIZE_ID)
+    this.setState({ variant })
+  }
+}
+
 export default props => {
+
+  useInterval(
+    checkGoogleOptimizeLoading,
+    window?.google_optimize ? 100 : null
+  )
+
   if (window?.location?.href?.includes('strove.io')) {
     return <Enterprise {...props} />
   } else {
