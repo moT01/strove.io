ada387a42e6b91aa94198161242b97704e96c8ca
adamzaczek <adamzaczekul@gmail.com>
2019-10-27 17:03:45 +0100

    Remove redundant console

--- src/components/home/banner/index.js
+++ src/components/home/banner/index.js
@@ -7,7 +7,6 @@ const B = lazy(() => import('./b'))
 
 export default memo(props => {
   const feature = useSelector(selectors.feature.getFeature)
-  console.log('feature', feature)
   return (
     <Suspense fallback={<div>Loading...</div>}>
       {feature.includes('b') ? <B {...props} /> : <A {...props} />}
