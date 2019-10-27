33f40314c0e19f4d7d8db6eb901b961d8179df22
adamzaczek <adamzaczekul@gmail.com>
2019-10-27 16:12:21 +0100

    Make features work

--- src/components/home/banner/index.js
+++ src/components/home/banner/index.js
@@ -1,25 +1,16 @@
-import React, { lazy, Suspense } from 'react'
-import { window } from 'utils'
-import { useInterval } from 'hooks'
+import React, { lazy, Suspense, memo } from 'react'
+import { useSelector } from 'react-redux'
+import { selectors } from 'state'
 
 const A = lazy(() => import('./a'))
 const B = lazy(() => import('./b'))
 
-const checkGoogleOptimizeLoading = () => {
-  if (window?.google_optimize !== undefined) {
-    const variant = window.google_optimize.get(process.env.GOOGLE_OPTIMIZE_ID)
-  }
-}
-
-export default props => {
-  useInterval(checkGoogleOptimizeLoading, window?.google_optimize ? 100 : null)
-
-  const searchParams = new URL(window?.location?.href).searchParams
-  const feature = searchParams?.get('feature') || ''
-
+export default memo(props => {
+  const feature = useSelector(selectors.feature.getFeature)
+  console.log('feature', feature)
   return (
     <Suspense fallback={<div>Loading...</div>}>
       {feature.includes('b') ? <B {...props} /> : <A {...props} />}
     </Suspense>
   )
-}
+})
