8bc18694f166a3cb24af00030776ef082df85024
adamzaczek <adamzaczekul@gmail.com>
2019-10-27 22:27:02 +0100

    More style cleanup

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,4 +1,5 @@
 import React, { memo } from 'react'
+import styled from 'styled-components'
 
 import Banner from './banner'
 import Features from './features'
@@ -6,13 +7,34 @@ import Technologies from './technologies'
 import Footer from './footer'
 import './static/style'
 
+const StyledWrapper = styled.div`
+  width: 100vw;
+  color: #697b8c;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  .ant-btn {
+    min-width: 110px;
+    height: 40px;
+    border-radius: 20px;
+    font-size: 16px;
+  }
+
+  .ant-btn:hover {
+    transform: translateY(-4px);
+    box-shadow: 0 4px 12px rgba(24, 144, 255, 0.4);
+  }
+`
+
 const Home = () => (
-  <div className="home-wrapper">
+  <StyledWrapper>
     <Banner />
     <Features />
     <Technologies />
     <Footer />
-  </div>
+  </StyledWrapper>
 )
 
 export default memo(Home)
