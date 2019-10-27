b51f0beb40b602e17755eadee4f56010228265ba
adamzaczek <adamzaczekul@gmail.com>
2019-10-27 02:25:45 +0200

    Stash changes

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,4 +1,6 @@
 import React, { memo } from 'react'
+import { window } from 'utils'
+import { useInterval } from 'hooks'
 
 import Banner from './banner'
 import Features from './features'
@@ -6,13 +8,24 @@ import Technologies from './technologies'
 import Footer from './footer'
 import './static/style'
 
-const Home = () => (
-  <div className="home-wrapper">
-    <Banner />
-    <Features />
-    <Technologies />
-    <Footer />
-  </div>
-)
+const checkGoogleOptimizeLoading = () => {
+  console.log('window?.google_optimize', window?.google_optimize)
+  if (window?.google_optimize !== undefined) {
+    const variant = window.google_optimize.get(process.env.GOOGLE_OPTIMIZE_ID)
+  }
+}
+
+const Home = () => {
+  useInterval(checkGoogleOptimizeLoading, !window?.google_optimize ? 100 : null)
+
+  return (
+    <div className="home-wrapper">
+      <Banner />
+      <Features />
+      <Technologies />
+      <Footer />
+    </div>
+  )
+}
 
 export default memo(Home)
