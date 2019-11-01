6c5040d3fb61bff739784afabdf90809931999ac
adamzaczek <adamzaczekul@gmail.com>
2019-11-01 11:36:48 +0100

    Extract reusable props from technologies

--- src/constants/technologies.js
+++ src/constants/technologies.js
@@ -1,29 +1,35 @@
 import React from 'react'
 import { Docker, Github, Google, VSCode } from 'images/logos'
 
+const defaultProps = {
+  style: { fill: '#303c42' },
+  width: '100%',
+  length: 'auto'
+}
+
 export default [
   {
     component: (
-      <Docker fill="#fff" style={{ fill: '#303c42' }} width="100%" length="auto" />
+      <Docker {...defaultProps} />
     ),
     name: 'Docker',
     description:
       'Strove.io represents each environment as a Docker container built from a shared image. This lets you code in seconds, on any computer and forget that `it works on my machine` issue ever existed.',
   },
   {
-    component: <Google style={{ fill: '#303c42' }} width="100%" length="auto" />,
+    component: <Google {...defaultProps}  />,
     name: 'Google',
     description:
       'Google Cloud allows us to host secure, consistent cloud environment.',
   },
   {
-    component: <VSCode style={{ fill: '#303c42' }} width="100%" length="auto" />,
+    component: <VSCode {...defaultProps} />,
     name: 'VSCode',
     description:
       'Strove.io supports VSCode to provide the best programming experience.',
   },
   {
-    component: <Github style={{ fill: '#303c42 ' }} width="100%" length="auto" />,
+    component: <Github {...defaultProps} />,
     name: 'Github',
     description:
       'Github, Gitlab and Bitbucket integration provide state-of-the-art version control.',
