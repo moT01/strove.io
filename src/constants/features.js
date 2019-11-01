81398899022fcbb1433096bd1b1b3baec2b4cdb7
adamzaczek <adamzaczekul@gmail.com>
2019-11-01 03:31:49 +0100

    Remove redundant values

--- src/constants/features.js
+++ src/constants/features.js
@@ -2,22 +2,18 @@ export default [
   {
     title: 'Run projects straight in the browser',
     content: 'No setup needed',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/aLQyKyUyssIUhHTZqCIb.svg',
     color: '#EB2F96',
     shadowColor: 'rgba(235,45,150,.12)',
-    type: 'chrome',
   },
   {
     title: 'Work with VSCode',
     content: 'You can even use your favorite extensions',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/smwQOoxCjXVbNAKMqvWk.svg',
     color: '#0072ce',
     shadowColor: 'rgba(0, 114, 206, 0.12)',
   },
   {
     title: 'Access your code anywhere',
     content: 'Log in to your account from any computer and code in the cloud',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/JsixxWSViARJnQbAAPkI.svg',
     color: '#13C2C2',
     shadowColor: 'rgba(19,194,194,.12)',
     type: 'global',
@@ -26,53 +22,41 @@ export default [
     title: 'Code in all major programming languages',
     content:
       'You can even use a particular language version. Need Python 2.7.3 for chemistry? We got you covered!',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/BISfzKcCNCYFmTYcUygW.svg',
     color: '#722ED1',
     shadowColor: 'rgba(114,46,209,.12)',
-    type: 'code',
   },
   {
     title: "Don't lose time, one environment for the whole team",
     content:
       'Once a project has been set up, any member can access it without needing to configure everything again',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/XxqEexmShHOofjMYOCHi.svg',
     color: '#FAAD14',
     shadowColor: 'rgba(250,173,20,.12)',
-    type: 'team',
   },
   {
     title: 'Build fullstack apps',
     content:
       'and save battery, CPU and RAM. Expensive computations are done in the cloud',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/hBbIHzUsSbSxrhoRFYzi.svg',
     color: '#F5222D',
     shadowColor: 'rgba(245,34,45,.12)',
-    type: 'appstore',
   },
   {
     title: "It's open-source",
     content: 'Contribute! Strove has been built with contributors in mind',
     href: 'https://github.com/stroveio/strove.io-client',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/pbmKMSFpLurLALLNliUQ.svg',
     color: '#FA8C16',
     shadowColor: 'rgba(250,140,22,.12)',
-    type: 'github',
   },
   {
     title: 'Push straight to GitHub, Gitlab or Bitbucket',
     content: 'Strove is made with seamless git experience in mind',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/VriUmzNjDnjoFoFFZvuh.svg',
     color: '#1AC44D',
     shadowColor: 'rgba(26,196,77,.12)',
-    type: 'cloud',
   },
   {
     title: "Say goodbye to 'it works on my machine'",
     content:
       'Thanks to docker based infrastructure every member of the team works on the same environment',
-    src: 'https://gw.alipayobjects.com/zos/rmsportal/RpJIQitGbSCHwLMimybX.svg',
     color: '#1890FF',
     shadowColor: 'rgba(24,144,255,.12)',
-    type: 'desktop',
   },
 ]
