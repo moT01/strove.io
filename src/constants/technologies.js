f8538d11eae87902012e84890b393d3c8a7a8240
adamzaczek <adamzaczekul@gmail.com>
2019-11-01 03:34:51 +0100

    Remove redundant assets

--- src/constants/technologies.js
+++ src/constants/technologies.js
@@ -4,26 +4,26 @@ import { Docker, Github, Google, VSCode } from 'images/logos'
 export default [
   {
     component: (
-      <Docker fill="#fff" style={{ fill: '#efefef' }} width="100%" length="auto" />
+      <Docker fill="#fff" style={{ fill: '#303c42' }} width="100%" length="auto" />
     ),
     name: 'Docker',
     description:
       'Strove.io represents each environment as a Docker container built from a shared image. This lets you code in seconds, on any computer and forget that `it works on my machine` issue ever existed.',
   },
   {
-    component: <Google style={{ fill: '#efefef' }} width="100%" length="auto" />,
+    component: <Google style={{ fill: '#303c42' }} width="100%" length="auto" />,
     name: 'Google',
     description:
       'Google Cloud allows us to host secure, consistent cloud environment.',
   },
   {
-    component: <VSCode style={{ fill: '#efefef' }} width="100%" length="auto" />,
+    component: <VSCode style={{ fill: '#303c42' }} width="100%" length="auto" />,
     name: 'VSCode',
     description:
       'Strove.io supports VSCode to provide the best programming experience.',
   },
   {
-    component: <Github style={{ fill: '#efefef' }} width="100%" length="auto" />,
+    component: <Github style={{ fill: '#303c42 ' }} width="100%" length="auto" />,
     name: 'Github',
     description:
       'Github, Gitlab and Bitbucket integration provide state-of-the-art version control.',
