b2c877537697d842fc47562630a98983c11130b5
adamzaczek <adamzaczekul@gmail.com>
2019-11-02 22:32:36 +0000

    Update technologies

--- src/constants/technologies.js
+++ src/constants/technologies.js
@@ -4,34 +4,32 @@ import { Docker, Github, Google, VSCode } from 'images/logos'
 const defaultProps = {
   style: { fill: '#303c42' },
   width: '100%',
-  length: 'auto'
+  length: 'auto',
 }
 
 export default [
   {
-    component: (
-      <Docker {...defaultProps} />
-    ),
+    component: <Docker {...defaultProps} />,
     name: 'Docker',
     description:
       'Strove.io represents each environment as a Docker container built from a shared image. This lets you code in seconds, on any computer and forget that `it works on my machine` issue ever existed.',
   },
   {
-    component: <Google {...defaultProps}  />,
-    name: 'Google',
-    description:
-      'Google Cloud allows us to host secure, consistent cloud environment.',
-  },
-  {
     component: <VSCode {...defaultProps} />,
     name: 'VSCode',
     description:
-      'Strove.io supports VSCode to provide the best programming experience.',
+      'Strove.io ensures the best programming experience by supporting Visual Studio Code, the most popular code editor in the world according to Stackoverflow.',
+  },
+  {
+    component: <Google {...defaultProps} />,
+    name: 'Google',
+    description:
+      'Code written in Strove is hosted and run in Google Cloud, ensuring consistency, security and the best performance',
   },
   {
     component: <Github {...defaultProps} />,
     name: 'Github',
     description:
-      'Github, Gitlab and Bitbucket integration provide state-of-the-art version control.',
+      'Github, Gitlab and Bitbucket integration provide state-of-the-art version control support allowing companies use their source control provider of choice.',
   },
 ]
