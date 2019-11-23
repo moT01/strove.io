a51c964aff7cf163f3175efbb0b9209b8bc201bb
adamzaczek <adamzaczekul@gmail.com>
2019-11-23 17:42:30 +0100

    Let Gatsby take control over the components structure because of its impossible to debug issues

--- src/pages/embed.js
+++ src/pages/embed.js
@@ -1,11 +1,9 @@
 import React, { memo } from 'react'
-import { Router } from '@reach/router'
 import { navigate } from 'gatsby'
 import { useSelector } from 'react-redux'
 
 import { selectors } from 'state'
 import { getWindowSearchParams, getWindowPathName } from 'utils'
-import { PrivateRoute, Editor, Embed } from 'components'
 
 const getToken = selectors.api.getUserField('siliskyToken')
 
@@ -13,23 +11,15 @@ const getToken = selectors.api.getUserField('siliskyToken')
 const EmbedWrapper = () => {
   const token = useSelector(getToken)
 
-  const searchParams = getWindowSearchParams()
-  const projectUrl = searchParams.get('projectUrl')
+  // const searchParams = getWindowSearchParams()
+  // const projectUrl = searchParams.get('projectUrl')
 
-  if (!token && getWindowPathName() !== `/embed/`) {
-    // If users is not logged in, redirect to the embed main page.
-    navigate('/embed/')
-  }
+  // if (!token && getWindowPathName() !== `/embed/`) {
+  //   // If users is not logged in, redirect to the embed main page.
+  //   navigate('/embed/')
+  // }
 
-  return (
-    <Router>
-      <Embed.Login path="/embed" />
-      <Embed.Login path="/embed/login" />
-      <Embed.Login path="/embed/goBackToOrigin" />
-      <Embed.Login path="/embed/run" />
-      <PrivateRoute path="/embed/editor" component={Editor} />
-    </Router>
-  )
+  return <div>Strove Embedded main page</div>
 }
 
 export default memo(EmbedWrapper)
