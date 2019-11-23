db2246ebece73bc0ffcc2946fda8f0b84af06933
adamzaczek <adamzaczekul@gmail.com>
2019-11-23 07:03:32 +0100

    Do not break app if theres no link, show info instead

--- src/pages/embed.js
+++ src/pages/embed.js
@@ -1,3 +1,42 @@
+// import React, { memo } from 'react'
+// import { Router } from '@reach/router'
+// import { navigate } from 'gatsby'
+// import { useSelector } from 'react-redux'
+
+// import { selectors } from 'state'
+// import { getWindowSearchParams, getWindowPathName } from 'utils'
+// import { PrivateRoute, Editor, Embed } from 'components'
+// import { Login, RunProject } from 'components/embed'
+
+// const getToken = selectors.api.getUserField('siliskyToken')
+
+// const EmbedWrapper = () => {
+//   const token = useSelector(getToken)
+
+//   const searchParams = getWindowSearchParams()
+//   const projectUrl = searchParams.get('projectUrl')
+
+//   // if (!token && getWindowPathName() !== `/embed/` && ) {
+//   //   // If users is not logged in, redirect to the embed main page.
+//   //   navigate('/embed/')
+//   // }
+//   // else if (token && window?.location?.pathname !== `/embed/editor/`) {
+//   //   navigate('/embed/run/')
+//   // }
+
+//   return (
+//     <Router>
+//       <Login path="/embed" />
+//       <Login path="/embed/login" />
+//       {/* <Embed.Login path="/embed/goBackToOrigin" /> */}
+//       <RunProject path="/embed/run" />
+//       <PrivateRoute path="/embed/editor" component={Editor} />
+//     </Router>
+//   )
+// }
+
+// export default memo(EmbedWrapper)
+
 import React, { memo } from 'react'
 import { Router } from '@reach/router'
 import { navigate } from 'gatsby'
@@ -6,15 +45,17 @@ import { useSelector } from 'react-redux'
 import { selectors } from 'state'
 import { getWindowSearchParams, getWindowPathName } from 'utils'
 import { PrivateRoute, Editor, Embed } from 'components'
-import { Login, RunProject } from 'components/embed'
 
 const getToken = selectors.api.getUserField('siliskyToken')
 
+/* Todo: Add embed only loading screen */
 const EmbedWrapper = () => {
   const token = useSelector(getToken)
 
   const searchParams = getWindowSearchParams()
   const projectUrl = searchParams.get('projectUrl')
+
+  console.log('getWindowPathName', getWindowPathName())
 
   // if (!token && getWindowPathName() !== `/embed/` && ) {
   //   // If users is not logged in, redirect to the embed main page.
@@ -26,10 +67,10 @@ const EmbedWrapper = () => {
 
   return (
     <Router>
-      <Login path="/embed" />
-      <Login path="/embed/login" />
-      {/* <Embed.Login path="/embed/goBackToOrigin" /> */}
-      <RunProject path="/embed/run" />
+      <Embed.Login path="/embed" />
+      <Embed.Login path="/embed/login" />
+      <Embed.Login path="/embed/goBackToOrigin" />
+      <Embed.Login path="/embed/run" />
       <PrivateRoute path="/embed/editor" component={Editor} />
     </Router>
   )
--- src/utils/getRepoProvider.js
+++ src/utils/getRepoProvider.js
@@ -1,4 +1,7 @@
 export default repoLink => {
+  if (!repoLink) {
+    return ''
+  }
   const repoUrlParts = repoLink.split('/')
   let repoProvider = repoUrlParts[2].split('.')[0]
   if (repoProvider.length > 6) {
