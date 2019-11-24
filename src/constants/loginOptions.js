c936b1662026d1063e33c3d7dae2799a6539831d
adamzaczek <adamzaczekul@gmail.com>
2019-11-25 00:49:39 +0100

    Update redirect mechanism

--- src/constants/loginOptions.js
+++ src/constants/loginOptions.js
@@ -8,6 +8,7 @@ const GITLAB_CLIENT_ID = process.env.GITLAB_CLIENT_ID
 const BITBUCKET_CLIENT_ID = process.env.BITBUCKET_CLIENT_ID
 const REDIRECT_URI = process.env.REDIRECT_URI
 const IS_OPENSOURCE = process.env.IS_OPENSOURCE
+const SILISKY_URL = process.env.SILISKY_URL
 
 /* This will catch route such as strove.io/app/dashboard/ */
 const adress = getWindowHref()?.split('?')[0]
@@ -19,21 +20,21 @@ export default [
     value: 'github',
     label: 'Github',
     href: `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user,user:email,public_repo&state=github,${IS_OPENSOURCE},${adress}`,
-    embedHref: `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user,user:email,public_repo&state=github,$true,https://strove.io/embed/goBackTo/`,
+    embedHref: `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user,user:email,public_repo&state=github,$true,${SILISKY_URL}goBackTo/`,
     icon: <Github />,
   },
   {
     value: 'bitbucket',
     label: 'Bitbucket',
     href: `https://bitbucket.org/site/oauth2/authorize?client_id=${BITBUCKET_CLIENT_ID}&response_type=code&state=bitbucket,${IS_OPENSOURCE},${adress}`,
-    embedHref: `https://bitbucket.org/site/oauth2/authorize?client_id=${BITBUCKET_CLIENT_ID}&response_type=code&state=bitbucket,true,https://strove.io/embed/goBackTo/`,
+    embedHref: `https://bitbucket.org/site/oauth2/authorize?client_id=${BITBUCKET_CLIENT_ID}&response_type=code&state=bitbucket,true,${SILISKY_URL}goBackTo/`,
     icon: <Bitbucket />,
   },
   {
     value: 'gitlab',
     label: 'Gitlab',
     href: `https://gitlab.com/oauth/authorize?client_id=${GITLAB_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&state=gitlab,${IS_OPENSOURCE},${adress}`,
-    embedHref: `https://gitlab.com/oauth/authorize?client_id=${GITLAB_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&state=gitlab,true,https://strove.io/embed/goBackTo/`,
+    embedHref: `https://gitlab.com/oauth/authorize?client_id=${GITLAB_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&state=gitlab,true,${SILISKY_URL}goBackTo/`,
     icon: <Gitlab />,
   },
 ]
