259768bd3599b03b9b4ee7ff6c952945c7e31dc1
adamzaczek <adamzaczekul@gmail.com>
2019-11-26 14:50:53 +0100

    Increase projects limit

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -165,12 +165,12 @@ const Dashboard = () => {
   const isContinuing = useSelector(selectors.api.getLoading('continueProject'))
   const currentProject = projects.find(item => item.machineId)
   const currentProjectId = currentProject && currentProject.id
-  const subscription = useSelector(
-    selectors.api.getApiData({ fields: 'subscription' })
-  )
+  // const subscription = useSelector(
+  //   selectors.api.getApiData({ fields: 'subscription' })
+  // )
 
-  const projectsLimit =
-    (subscription.status === 'active' && subscription.projects_limit) || 4
+  const projectsLimit = 20
+  // (subscription.status === 'active' && subscription.projects_limit) || 4
 
   const handleStartClick = ({ id, editorPort }) => {
     if (!currentProjectId || currentProjectId === id) {
