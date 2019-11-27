2a169ebfaec6b278856d8e84d0838ff8564545b0
adamzaczek <adamzaczekul@gmail.com>
2019-11-27 23:53:01 +0100

    Do not clone repoUrl by default projects

--- src/utils/getRepoProvider.js
+++ src/utils/getRepoProvider.js
@@ -3,6 +3,7 @@ export default repoLink => {
     return ''
   }
   const repoUrlParts = repoLink.split('/')
+
   let repoProvider = repoUrlParts[2].split('.')[0]
   if (repoProvider.length > 6) {
     repoProvider = repoProvider.split('@')[1]
