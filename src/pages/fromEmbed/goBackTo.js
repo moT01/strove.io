1209e4178031ed2b2b50d4a52958013e074e923e
adamzaczek <adamzaczekul@gmail.com>
2019-12-03 00:30:23 +0100

    Make the flow work properly

--- src/pages/fromEmbed/goBackTo.js
+++ src/pages/fromEmbed/goBackTo.js
@@ -29,11 +29,14 @@ const GoBackTo = () => {
   const searchParams = getWindowSearchParams()
   const goBackTo = searchParams.get('goBackTo')
 
-  console.log('goBackTo', goBackTo, 'token', token)
-
-  /* Redirect logged in users */
   if (goBackTo && token) {
-    window.location.replace(goBackTo)
+    /*
+      Redirect logged in users
+      Redirect should only happen once logged in state is saved to localStorage.
+      ToDo: Find a better way to do this. One of the ways includes reacting to storage events:
+      https://developer.mozilla.org/en-US/docs/Web/API/StorageEvent
+    */
+    setTimeout(() => window.location.replace(goBackTo), 1000)
   }
 
   return (
