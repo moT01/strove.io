73f7465308c1f86c287a8528b0ac7e715d553ecb
MateuszOlek <olek.mateusz@gmail.com>
2019-12-04 16:56:00 +0100

    Stash changes

---
+++ src/components/deployModal.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react'
+import styled, { keyframes } from 'styled-components'
+import { isMobileOnly } from 'react-device-detect'
+
+import StroveButton from 'components/stroveButton.js'
+
+const DeployModal = () => {
+  return <div />
+}
+
+export default memo(DeployModal)
