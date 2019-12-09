e13cbe37e60c6035600c10230dbaaa00f0375730
adamzaczek <adamzaczekul@gmail.com>
2019-12-09 21:34:18 +0100

    More styling

--- src/components/home/enterprise.js
+++ src/components/home/enterprise.js
@@ -12,8 +12,29 @@ const EnterpriseWrapper = styled.div`
   padding: 40px 0;
 `
 
+const EnterpriseFeatures = styled.ul`
+  background-color: ${({ theme }) => theme.colors.c3};
+  padding: 20px;
+  list-style: none;
+  min-width: 320px;
+  text-align: left;
+  font-size: 20px;
+  li:before {
+    margin-right: 0.3em;
+    content: '✔';
+    color: ${({ theme }) => theme.colors.c1};
+  }
+`
+
 const Footer = () => (
   <EnterpriseWrapper>
+    <EnterpriseFeatures>
+      <li>No need to buy computers</li>
+      <li>Less bureaucracy</li>
+      <li>Less money spent</li>
+      <li>More code</li>
+      <li>Powerful servers</li>
+    </EnterpriseFeatures>
     Enterprise
   </EnterpriseWrapper>
 )
