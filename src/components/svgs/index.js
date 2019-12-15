3bd4f9f58bdc4f3c83750ef1f2bcfec5d702da86
adamzaczek <adamzaczekul@gmail.com>
2019-12-15 23:27:35 +0100

    Add some svg imports

---
+++ src/components/svgs/index.js
@@ -0,0 +1,45 @@
+// import Typescript from './typescript'
+// import Cpp from './cpp'
+// import Go from './go'
+// import Java from './java'
+// import Ruby from './ruby'
+// import CLogo from './c'
+// import Javascript from './javascript'
+// import Python from './python'
+// import CSharp from './csharp'
+// import Node from './node'
+import VSCode from './vscode'
+import Google from './google'
+import Github from './github'
+import Docker from './docker'
+import Strove from './strove'
+import Bitbucket from './bitbucket'
+import Gitlab from './gitlab'
+// import Php from './php'
+// import C from './c'
+import Dashboard from './dashboard'
+import Desktop from './desktop'
+
+export {
+  // Typescript,
+  // Cpp,
+  // Go,
+  // Java,
+  // Ruby,
+  // CLogo,
+  // Javascript,
+  // Python,
+  // CSharp,
+  // Node,
+  VSCode,
+  Google,
+  Github,
+  Docker,
+  Strove,
+  Bitbucket,
+  Gitlab,
+  // Php,
+  // C,
+  Dashboard,
+  Desktop,
+}
