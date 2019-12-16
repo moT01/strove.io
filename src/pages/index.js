be2fd8a62ceb6133b9950947c3a4998cd77082d8
adamzaczek <adamzaczekul@gmail.com>
2019-12-16 02:06:42 +0100

    Add missing imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Home from './home'
 import Faq from './faq'
 import Embed from './embed'
-import NotFound from './embed'
-// import Embed from './embed'
-// import Embed from './embed'
-// import Embed from './embed'
-// import Embed from './embed'
-// import Embed from './embed'
+import NotFound from './notFound'
+import Cookies from './cookies'
+import PrivacyPolicy from './privacyPolicy'
+import TermsAndConditions from './termsAndConditions'
+import FromEmbed from './fromEmbed'
+import App from './app'
 
 export { Home, Faq, Embed }
