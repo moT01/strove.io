4fbffe634c44eb353e54875bddbfe637fdf7b8f7
adamzaczek <adamzaczekul@gmail.com>
2019-12-16 19:34:22 +0100

    Use a for external link

--- src/components/home/features.js
+++ src/components/home/features.js
@@ -7,7 +7,6 @@ import { Icon } from 'antd'
 import { VSCode } from 'components/svgs'
 import styled from 'styled-components/macro'
 import { isMobileOnly } from 'react-device-detect'
-import { Link } from 'react-router-dom'
 
 import { features } from 'consts'
 
@@ -23,7 +22,7 @@ const StyledFeatureContent = styled.p`
   font-size: 14px;
 `
 
-const StyledAnchor = styled(Link)`
+const StyledAnchor = styled.a`
   text-decoration: none;
   color: rgb(105, 123, 140);
 `
