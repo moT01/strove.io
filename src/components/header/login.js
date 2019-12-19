8689a7a4297e6ec4bd426a69281a63ff04a470ba
adamzaczek <adamzaczekul@gmail.com>
2019-12-19 18:41:04 +0100

    Simplify styles

--- src/components/header/login.js
+++ src/components/header/login.js
@@ -52,13 +52,12 @@ const MenuWrapper = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   width: auto;
-  box-shadow: 0 1.2vh 1.2vh -1.5vh ${({ theme }) => theme.colors.c1};
+  box-shadow: 0 1.2vh 1.2vh -1.5vh ${({ theme }) => theme.colors.c2};
   border-radius: 5px;
   border-width: 1px;
   border-color: ${({ theme }) => theme.colors.c1};
   border-style: solid;
-  background-color: ${({ theme, invert }) =>
-    invert ? theme.colors.c2 : theme.colors.c1};
+  background-color: ${({ theme }) => theme.colors.c2};
   z-index: 3;
   position: relative;
 `
@@ -189,7 +188,7 @@ const LoginDropdown = () => {
             <Text isEmbed={isEmbed}>Login</Text>
           </LoginButton>
           <DropdownWrapper hidden={!isOpen}>
-            <MenuWrapper invert>
+            <MenuWrapper>
               {loginOptions.map((item, index) => (
                 <Option
                   invert
@@ -234,7 +233,7 @@ const UserDropdown = props => {
             </StyledDropdown>
           </Wrapper>
           <DropdownWrapper hidden={!isOpen}>
-            <MenuWrapper invert>
+            <MenuWrapper>
               <Option
                 isLast
                 onClick={() => {
