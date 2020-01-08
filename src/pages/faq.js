29a68b321c51ea1b668c2ccc02e99eb6320b01c0
adamzaczek <adamzaczekul@gmail.com>
2020-01-08 18:04:13 +0100

    Add additional points to first question

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -79,13 +79,13 @@ const topics = [
       1. Companies need to buy and manage expensive equipment for each developer.
       Strove allows developers to work on their own computers while still allowing companies to secure code or even, recover it.
       2. It's hard to secure the code.
-      Storing code on remote servers allows to easily restrict access or recover code.
+      Storing code on remote servers allows to recover code or restrict access.
       3. Programmers often waste a lot of time on setting up the environment.
-      Strove.io removed the need to set a programming environment for each and every developer.
-      2. It is hard to use different computers when working on a project and code breaks across different devices and operating systems.
+      Strove.io removes the need to set a programming environment for each and every developer.
+      4. It is hard to use different computers when working on a project and code breaks across different devices and operating systems.
       In Strove.io the code lives in a cloud. This makes it possible to resume working on a project from any computer with internet access.
-      3. 'It works on my machine'.
-      Strove.io lets programmers use their favorite devices without sacrificing productivity. All team members access instances of the same programming environment, no matter if they use Macbook or an old PC.
+      5. 'It works on my machine'.
+      All team members access instances of the same programming environment, and the code works the same way no matter the computer they use.
     `,
   },
   {
