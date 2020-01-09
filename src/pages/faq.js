7932f3de046a414bf2d12c7215a8f8b2b5651e0e
adamzaczek <adamzaczekul@gmail.com>
2020-01-09 17:23:31 +0100

    Faq updates

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -82,7 +82,7 @@ const StyledAccordionItemPanel = styled(AccordionItemPanel)`
 const topics = [
   {
     header: 'What is Strove.io?',
-    paragraph: `Strove allows to write, run, build, share and secure code using cloud servers.
+    paragraph: `Strove exposes cloud servers to allow developers to write, run, build, share and secure software.
     It uses the most popular code editor VSCode. This means that it can provide a local-quality development experience — including full IntelliSense (completions), debugging, and more.
     Strove provides consistent development, allowing programmers to use any device without worrying about 'it works on my machine' issue.`,
   },
