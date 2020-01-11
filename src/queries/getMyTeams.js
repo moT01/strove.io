a09f8fe9fb3a6de81510d7861ea48a414b702376
MateuszOlek <olek.mateusz@gmail.com>
2020-01-11 13:06:37 +0100

    Quick save

--- src/queries/getMyTeams.js
+++ src/queries/getMyTeams.js
@@ -1,16 +1,11 @@
 import gql from 'graphql-tag'
 import { TeamFragment } from './fragments/team'
 
-// export default gql`
-// 	query getMyTeams {
-// 			...Team
-// 	}
-
-// 	${TeamFragment}
-// `
-
 export default gql`
-{ getMyTeams { ...Team } }
+	query GetMyTeams { getMyTeams {
+		...Team
+		}
+	}
 
-${TeamFragment}
-`
+	${TeamFragment}
+	`
