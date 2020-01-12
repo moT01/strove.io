16b329edf92e9f86c1ed27c8bb2191b1d1dfc71f
adamzaczek <adamzaczekul@gmail.com>
2020-01-12 02:05:43 +0100

    Do not show cookies in embed

--- src/components/cookies.js
+++ src/components/cookies.js
@@ -7,10 +7,11 @@ const StyledCookieConsent = styled(CookieConsent)`
   background: #2b373b;
 `
 
-const Cookies = () => (
-  <StyledCookieConsent buttonText="Understood">
-    This website uses cookies to enhance the user experience.
-  </StyledCookieConsent>
-)
+const Cookies = () =>
+  !window.location.href.includes('/embed') && (
+    <StyledCookieConsent debug buttonText="Understood">
+      This website uses cookies to enhance the user experience.
+    </StyledCookieConsent>
+  )
 
 export default memo(Cookies)
