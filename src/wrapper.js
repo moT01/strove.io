83a2a42a64ecc8c6860ad52701c9189f28eeccac
adamzaczek <adamzaczekul@gmail.com>
2020-01-12 01:47:44 +0100

    Add cookies component

--- src/wrapper.js
+++ src/wrapper.js
@@ -7,6 +7,7 @@ import thunk from 'redux-thunk'
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import CookieConsent from 'react-cookie-consent'
 
 import { getWindowSearchParams } from 'utils'
 import { Layout } from 'components'
@@ -40,7 +41,19 @@ export default ({ children }) => (
       <Provider store={createStore}>
         <PersistGate loading={null} persistor={persistor}>
           <WithAnalyticsWrapper>
-            <Layout>{children}</Layout>
+            <Layout>
+              {children}
+              <CookieConsent
+                location="bottom"
+                buttonText="Sure man!!"
+                cookieName="myAwesomeCookieName2"
+                style={{ background: '#2B373B' }}
+                buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
+                expires={150}
+              >
+                This website uses cookies to enhance the user experience.{' '}
+              </CookieConsent>
+            </Layout>
           </WithAnalyticsWrapper>
         </PersistGate>
       </Provider>
