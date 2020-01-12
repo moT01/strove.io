b14379dff6d9dafa8e4c3c871344dbb8949666b0
adamzaczek <adamzaczekul@gmail.com>
2020-01-12 01:51:40 +0100

    Clean up cookies component

--- src/components/cookies.js
+++ src/components/cookies.js
@@ -3,14 +3,14 @@ import styled from 'styled-components/macro'
 
 import CookieConsent from 'react-cookie-consent'
 
+const StyledCookieConsent = styled(CookieConsent)`
+  background: #2b373b;
+`
+
 const Cookies = () => (
-  <CookieConsent
-    buttonText="Understood"
-    style={{ background: '#2B373B' }}
-    buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
-  >
+  <StyledCookieConsent buttonText="Understood">
     This website uses cookies to enhance the user experience.{' '}
-  </CookieConsent>
+  </StyledCookieConsent>
 )
 
-export default Cookies
+export default memo(Cookies)
