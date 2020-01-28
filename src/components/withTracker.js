9313c8f298be879938f3b5184e170b2d97ab6f7a
adamzaczek <adamzaczekul@gmail.com>
2020-01-28 09:11:35 +0100

    Measure load times

--- src/components/withTracker.js
+++ src/components/withTracker.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import ReactGA from 'react-ga'
 
 ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID)
@@ -13,11 +13,35 @@ const withTracker = (WrappedComponent, options = {}) => {
   }
 
   const HOC = props => {
+    const intersectTarget = useRef(null)
+
+    useEffect(() => {
+      const opts = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0,
+      }
+      const callback = list => {
+        list.forEach(entry => {
+          if (entry.isIntersecting) {
+            ReactGA.event({
+              category: 'Scroll',
+              action: 'Scrolled to heading 2',
+              value: entry.intersectionRatio,
+            })
+          }
+        })
+      }
+      const observerScroll = new IntersectionObserver(callback, opts)
+
+      observerScroll.observe(intersectTarget.current)
+    }, [])
+
     useEffect(() => trackPage(props.location.pathname), [
       props.location.pathname,
     ])
 
-    return <WrappedComponent {...props} />
+    return <WrappedComponent ref={intersectTarget} {...props} />
   }
 
   return HOC
