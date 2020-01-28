767e19c5673ae00c016f68f08db8f59da4a15246
adamzaczek <adamzaczekul@gmail.com>
2020-01-28 09:57:02 +0100

    Cleanup

--- src/components/withTracker.js
+++ src/components/withTracker.js
@@ -13,28 +13,6 @@ const withTracker = (WrappedComponent, options = {}) => {
   }
 
   const HOC = props => {
-    useEffect(() => {
-      const opts = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0,
-      }
-      const callback = list => {
-        list.forEach(entry => {
-          if (entry.isIntersecting) {
-            ReactGA.event({
-              category: 'Rendering',
-              variable: entry.name,
-              value: entry.startTime,
-            })
-          }
-          console.log('entry.intersectionRatio', entry.startTime)
-        })
-        var observer = new PerformanceObserver(callback)
-        observer.observe({ entryTypes: ['paint'] })
-      }
-    }, [])
-
     useEffect(() => trackPage(props.location.pathname), [
       props.location.pathname,
     ])
