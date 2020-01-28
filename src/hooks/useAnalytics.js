40d2f1509cc099a805e49c8acff89196c1413735
adamzaczek <adamzaczekul@gmail.com>
2020-01-28 09:58:00 +0100

    Cleanup

--- src/hooks/useAnalytics.js
+++ src/hooks/useAnalytics.js
@@ -1,15 +1,9 @@
 import { useEffect, useRef } from 'react'
 import ReactGA from 'react-ga'
 
-/* Handle clearing interval */
 export default () => {
   const ref = useRef()
   useEffect(() => {
-    const opts = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0,
-    }
     const callback = list => {
       list.forEach(entry => {
         if (entry.isIntersecting) {
