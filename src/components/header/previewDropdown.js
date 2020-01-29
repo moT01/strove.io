712daa65bf3344094949d23ae835bcde36897969
Mateusz Olek <mateusz@codengo.page>
2020-01-29 13:09:19 +0100

    Preview fix

--- src/components/header/previewDropdown.js
+++ src/components/header/previewDropdown.js
@@ -190,13 +190,13 @@ const PreviewDropdown = props => {
             if (process.env.REACT_APP_IS_OPENSOURCE === 'true') {
               href = `https://${portPair[1]}.vmopen${
                 project.machineName.match(/\d+/g)[0]
-              }.silisky.com`
+              }.strove.io`
             } else if (process.env.NODE_ENV === 'development') {
               href = `https://${portPair[1]}.vmdev${
                 project.machineName.match(/\d+/g)[0]
-              }.silisky.com`
+              }.strove.io`
             } else {
-              href = `https://${portPair[1]}.${project.machineName}.silisky.com`
+              href = `https://${portPair[1]}.${project.machineName}.strove.io`
             }
             return {
               label: `http://0.0.0.0:${portPair[0]}`,
