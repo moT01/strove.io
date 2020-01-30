d4ee862570334af778f7fc3f74b8120d86d09d01
AdamZaczek <adamzaczekul@gmail.com>
2020-01-30 01:47:59 +0100

    Make observer work

--- src/hooks/useAnalytics.js
+++ src/hooks/useAnalytics.js
@@ -3,10 +3,10 @@ import ReactGA from 'react-ga'
 
 export default () => {
   console.log('hello')
-  const ref = useRef()
+  const ref = useRef(null)
   useEffect(() => {
     const callback = list => {
-      list.forEach(entry => {
+      list.getEntries().forEach(entry => {
         if (entry.isIntersecting) {
           ReactGA.event({
             category: 'Rendering',
@@ -16,9 +16,9 @@ export default () => {
         }
         console.log('entry.intersectionRatio', entry.startTime)
       })
-      const observer = new PerformanceObserver(callback)
-      observer.observe({ entryTypes: ['paint'] })
     }
+    const observer = new PerformanceObserver(callback)
+    observer.observe({ entryTypes: ['paint'] })
   }, [])
 
   return ref
