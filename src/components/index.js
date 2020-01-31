bd83992efe3e021aa9156fde1ba5c9e3c948788a
AdamZaczek <adamzaczekul@gmail.com>
2020-02-01 00:22:41 +0100

    Delete redundant component

---
+++ src/components/index.js
@@ -0,0 +1,51 @@
+import { SmallCloud, MediumCloud, BigCloud } from './clouds'
+import Header from './header'
+import SEO from './seo'
+import AddProjectModals from './addProjectModals'
+import AddProjectProvider from './addProjectProvider'
+import FullScreenLoader from './fullScreenLoader'
+import GetStarted from './getStarted'
+import Layout from './layout'
+import Logos from './logos.js'
+import Modal from './modal'
+import PrivateRoute from './privateRoute'
+import GlobalStyles from './globalStyles'
+import DataManager from './dataManager'
+import WithAddProject from './withAddProject'
+import Home from './home'
+import StroveButton from './stroveButton'
+import NoRepoUrlInfo from './NoRepoUrlInfo'
+import PoweredBy from './poweredBy'
+import ExternalLink from './externalLink'
+import ScrollToTop from './scrollToTop'
+import Cookies from './cookies'
+import WithTracker from './withTracker'
+import Footer from './footer'
+
+export {
+  SmallCloud,
+  MediumCloud,
+  BigCloud,
+  Header,
+  SEO,
+  AddProjectModals,
+  AddProjectProvider,
+  FullScreenLoader,
+  GetStarted,
+  Layout,
+  Logos,
+  Modal,
+  PrivateRoute,
+  GlobalStyles,
+  DataManager,
+  WithAddProject,
+  WithTracker,
+  Home,
+  StroveButton,
+  NoRepoUrlInfo,
+  PoweredBy,
+  ExternalLink,
+  ScrollToTop,
+  Cookies,
+  Footer,
+}
