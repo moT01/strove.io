d3de9378fef7504822669508b99829325377d41d
AdamZaczek <adamzaczekul@gmail.com>
2020-02-01 13:54:27 +0100

    Extract email form wrapper

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -36,6 +36,8 @@ import StroveLogo from 'images/strove.png'
 import PersonalTeam from './personalTeam'
 import Projects from './projects'
 
+import { EmailFormWrapper } from './styled'
+
 const validate = values => {
   let errors = {}
 
@@ -68,85 +70,6 @@ const FullFadeIn = keyframes`
   }
   100% {
     opacity: 1;
-  }
-`
-
-const EmailFormWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  min-width: 400px;
-  flex-wrap: wrap;
-  margin: 20px 0 5px;
-  position: relative;
-  box-shadow: 0 2px 4px 0 rgba(174, 174, 186, 0.24),
-    0 8px 24px 0 rgba(174, 174, 186, 0.16);
-  background: ${({ theme }) => theme.colors.c2};
-  display: flex;
-  flex-wrap: wrap;
-  position: relative;
-  border-radius: 5px;
-  transition: all 0.2s ease;
-  opacity: 0.9;
-  align-items: center;
-  ${({ isMobile }) =>
-    isMobile &&
-    css`
-      flex-direction: column;
-      box-shadow: none;
-      min-width: 100px;
-      border-radius: 5px;
-    `}
-  &:hover {
-    opacity: 1;
-    box-shadow: 0 3px 5px 0 rgba(174, 174, 186, 0.24),
-      0 9px 26px 0 rgba(174, 174, 186, 0.16);
-    ${({ isMobile }) =>
-      isMobile &&
-      css`
-        box-shadow: none;
-      `}
-  }
-  input {
-    box-shadow: none;
-    color: ${({ theme }) => theme.colors.c12};
-    outline: 0;
-    background: ${({ theme }) => theme.colors.c2};
-    width: calc(100% - 156px);
-    height: 56px;
-    padding: 0;
-    padding-left: ${({ isInvite }) => (isInvite ? '64px' : '8px')};
-    padding-top: 10px;
-    padding-bottom: 10px;
-    line-height: 36px;
-    font-size: 17px;
-    font-weight: normal;
-    font-style: normal;
-    font-stretch: normal;
-    letter-spacing: 0.2px;
-    border: 0;
-    border-radius: 5px;
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
-    ${({ isMobile }) =>
-      isMobile &&
-      css`
-        flex-direction: column;
-        box-shadow: 0 2px 4px 0 rgba(174, 174, 186, 0.24),
-          0 8px 24px 0 rgba(174, 174, 186, 0.16);
-        border-radius: 5px;
-        width: 100%;
-      `}
-  }
-  svg {
-    position: absolute;
-    top: 18px;
-    left: 20px;
-    height: 24px;
-    width: 24px;
-    g {
-      stroke: ${({ theme }) => theme.colors.c1};
-    }
   }
 `
 
