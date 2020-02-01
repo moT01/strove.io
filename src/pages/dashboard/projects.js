29aecb9cb660a1f1602302abd8833628ed9273fe
AdamZaczek <adamzaczekul@gmail.com>
2020-02-01 19:48:13 +0100

    Make hovering look better

--- src/pages/dashboard/projects.js
+++ src/pages/dashboard/projects.js
@@ -33,10 +33,12 @@ const Tile = styled.div`
   border-color: ${({ theme }) => theme.colors.c4};
   border-width: 0px 0px 1px 0px;
   border-style: solid;
-  margin: 5px;
-  padding-left: 10px;
   width: 100%;
   transition: all 0.2s;
+
+  :hover {
+    background-color: ${({ theme }) => theme.colors.c24};
+  }
 
   :last-of-type {
     border: none;
