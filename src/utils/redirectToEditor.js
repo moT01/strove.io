0bfdd9cdba0ceea04100bc0e3b56225cdb4bbd7a
Adam Żaczek <adamzaczekul@gmail.com>
2020-02-06 20:24:43 +0100

    Always redirect to editor on project start

--- src/utils/redirectToEditor.js
+++ src/utils/redirectToEditor.js
@@ -6,13 +6,11 @@ import { getWindowPathName, getWindowSearchParams } from './windowUtils'
 export default (dispatch, history) => {
   dispatch(actions.incomingProject.removeIncomingProject())
   const path = getWindowPathName()
-  if (document.visibilityState === 'visible') {
-    if (path.includes('embed') && document.visibilityState === 'visible') {
-      const searchParams = getWindowSearchParams()
-      const repoUrl = searchParams.get('repoUrl')
-      history.push(`/embed/editor/?repoUrl=${repoUrl}`)
-    } else {
-      history.push('/app/editor/')
-    }
+  if (path.includes('embed')) {
+    const searchParams = getWindowSearchParams()
+    const repoUrl = searchParams.get('repoUrl')
+    history.push(`/embed/editor/?repoUrl=${repoUrl}`)
+  } else {
+    history.push('/app/editor/')
   }
 }
