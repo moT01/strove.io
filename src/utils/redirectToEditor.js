5aba994ace40fb92b9ad8d60bcfb848b9a9ab00a
AdamZaczek <adamzaczekul@gmail.com>
2020-02-06 19:46:48 +0100

    Update redirect comments

--- src/utils/redirectToEditor.js
+++ src/utils/redirectToEditor.js
@@ -2,12 +2,12 @@ import { actions } from 'state'
 
 import { getWindowPathName, getWindowSearchParams } from './windowUtils'
 
-/* NO_GATSBY_TODO Pass history */
 export default (dispatch, history) => {
   dispatch(actions.incomingProject.removeIncomingProject())
   const path = getWindowPathName()
-  /* If we are to prevent redirecting to editor, we can do so with the following line */
-  // if (document.visibilityState === 'visible') {
+  /* If we are to prevent redirecting to editor, we can do so with the following line
+    if (document.visibilityState === 'visible') {
+  */
   if (path.includes('embed')) {
     const searchParams = getWindowSearchParams()
     const repoUrl = searchParams.get('repoUrl')
