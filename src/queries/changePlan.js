ca491d3bf5d2b682802cb0c37b0df4a6b1f8de71
MateuszOlek <olek.mateusz@gmail.com>
2020-02-07 15:16:31 +0100

    Added changePlan mutation

---
+++ src/queries/changePlan.js
@@ -0,0 +1,11 @@
+import gql from 'graphql-tag'
+import { OrganizationFragment } from './fragments/organization'
+
+export default gql`
+  mutation ChangePlan($organizationId: ID!, $newPlan: String!) {
+    changePlan(organizationId: $organizationId, newPlan: $newPlan) {
+      ...Organization
+    }
+  }
+  ${OrganizationFragment}
+`
