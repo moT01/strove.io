b8ffecb316aa842717f691fa4d46b7753a66ce0e
AdamZaczek <adamzaczekul@gmail.com>
2020-02-09 17:38:37 +0100

    Add menuwrapper

---
+++ src/components/menuWrapper.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  padding: 20px;
+  background-color: ${({ theme }) => theme.colors.c2};
+  z-index: 3;
+  position: relative;
+`
