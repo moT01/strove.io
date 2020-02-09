e272983953587f356f6e96ef04f8f8b41bdd531b
AdamZaczek <adamzaczekul@gmail.com>
2020-02-09 14:01:57 +0100

    Fix how loader looks

--- src/components/withLazyLoader.js
+++ src/components/withLazyLoader.js
@@ -1,11 +1,21 @@
 import React, { Suspense } from 'react'
+import styled from 'styled-components'
 
 import FullScreenLoader from './fullScreenLoader'
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`
 
 export default WrappedComponent => props => (
   <Suspense
     fallback={
-      <FullScreenLoader isFullScreen={false} color="#0072ce" height="15vh" />
+      <Wrapper>
+        <FullScreenLoader isFullScreen={false} color="#0072ce" height="15vh" />
+      </Wrapper>
     }
   >
     <WrappedComponent {...props} />
