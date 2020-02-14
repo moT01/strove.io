f9a042f7eff7df019eb1661cdea5879842fa0776
adamzaczek <adamzaczekul@gmail.com>
2020-02-15 00:14:05 +0100

    Make separate enterprise features const

--- src/consts/index.js
+++ src/consts/index.js
@@ -2,5 +2,6 @@ import theme from './theme'
 import devFeatures from './devFeatures'
 import technologies from './technologies'
 import loginOptions from './loginOptions'
+import enterpriseFeatures from './enterpriseFeatures'
 
-export { theme, devFeatures, technologies, loginOptions }
+export { theme, devFeatures, technologies, loginOptions, enterpriseFeatures }
