bfd3456750c7a2c33e91dde7ee326939f18826f5
adamzaczek <adamzaczekul@gmail.com>
2020-02-14 19:49:38 +0100

    Move styles to styled components completely

--- src/components/home/banner.js
+++ src/components/home/banner.js
@@ -18,6 +18,26 @@ import {
   StyledCellHeader,
   StyledSectionWrapper,
   StyledIcon,
+  StyledHeadingSection,
+  StyledH1,
+  ButtonsWrapper,
+  StyledProductDescription,
+  StyledInfo,
+  StyledForm,
+  EmailFormWrapper,
+  Illustration,
+  SectionDivider,
+  IconContainer,
+  StyledH3,
+  SectionWrapper,
+  SmallSectionWrapper,
+  StyledCell,
+  StyledH2,
+  StyledGrid,
+  StyledHeaderText,
+  StyledFeatureDescription,
+  StyledSmallText,
+  StyledTechnologyDescriptionWrapper,
 } from './styled'
 
 const validate = values => {
@@ -31,248 +51,6 @@ const validate = values => {
 
   return errors
 }
-
-const SectionDivider = styled.div`
-  display: flex;
-  flex-direction: ${({ isMobile, flexDirection }) =>
-    flexDirection || (isMobile ? 'column' : 'row')};
-  align-items: center;
-  height: 100%;
-  width: 100%;
-  max-width: 1200px;
-`
-
-const SectionWrapper = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-  max-width: 1200px;
-  padding: ${({ padding }) => padding || '0'};
-`
-
-const SmallSectionWrapper = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-  max-width: 440px;
-  max-height: 440px;
-  padding: ${({ padding }) => padding || '0'};
-`
-
-const ButtonsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: auto;
-  align-items: center;
-  justify-content: space-around;
-`
-
-const EmailFormWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  min-width: 400px;
-  flex-wrap: wrap;
-  margin: 20px 0 5px;
-  position: relative;
-  box-shadow: 0 2px 4px 0 rgba(174, 174, 186, 0.24),
-    0 8px 24px 0 rgba(174, 174, 186, 0.16);
-  background: ${({ theme }) => theme.colors.c2};
-  display: flex;
-  flex-wrap: wrap;
-  position: relative;
-  border-radius: 5px;
-  transition: all 0.2s ease;
-  opacity: 0.9;
-  align-items: center;
-  ${({ isMobile }) =>
-    isMobile &&
-    css`
-      flex-direction: column;
-      box-shadow: none;
-      min-width: 100px;
-      border-radius: 5px;
-    `}
-  &:hover {
-    opacity: 1;
-    box-shadow: 0 3px 5px 0 rgba(174, 174, 186, 0.24),
-      0 9px 26px 0 rgba(174, 174, 186, 0.16);
-    ${({ isMobile }) =>
-      isMobile &&
-      css`
-        box-shadow: none;
-      `}
-  }
-  input {
-    box-shadow: none;
-    color: ${({ theme }) => theme.colors.c12};
-    outline: 0;
-    background: ${({ theme }) => theme.colors.c2};
-    width: calc(100% - 156px);
-    height: 56px;
-    padding: 0;
-    padding-left: 64px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    line-height: 36px;
-    font-size: 17px;
-    font-weight: normal;
-    font-style: normal;
-    font-stretch: normal;
-    letter-spacing: 0.2px;
-    border: 0;
-    border-radius: 5px;
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
-    ${({ isMobile }) =>
-      isMobile &&
-      css`
-        flex-direction: column;
-        box-shadow: 0 2px 4px 0 rgba(174, 174, 186, 0.24),
-          0 8px 24px 0 rgba(174, 174, 186, 0.16);
-        border-radius: 5px;
-        width: 100%;
-      `}
-  }
-  svg {
-    position: absolute;
-    top: 18px;
-    left: 20px;
-    height: 24px;
-    width: 24px;
-    g {
-      stroke: ${({ theme }) => theme.colors.c1};
-    }
-  }
-`
-
-const StyledH2 = styled.h2`
-  margin: 0;
-  font-size: 36px;
-  width: 100%;
-  text-align: center;
-  text-transform: uppercase;
-  color: ${({ color, theme }) => color || theme.colors.c2};
-`
-
-const StyledH3 = styled.h3`
-  margin: 0;
-  font-size: 32px;
-  width: 100%;
-  text-align: center;
-  color: ${({ color, theme }) => color || theme.colors.c26};
-`
-
-const Video = styled.video`
-  width: ${props => (props.isMobile ? '90vw' : 'calc(100% - 40px)')};
-  margin-top: ${props => (props.isMobile ? '5vh' : '0')};
-  outline: none;
-`
-
-const StyledForm = styled(Form)`
-  width: 100%;
-`
-
-const StyledInfo = styled.span`
-  margin: 20px;
-  color: ${({ theme }) => theme.colors.c13};
-  font-size: 13px;
-`
-
-const StyledH1 = styled.h1`
-  margin-bottom: 10px;
-  color: ${({ theme }) => theme.colors.c3};
-  font-weight: 700;
-  font-size: 40px;
-`
-
-const StyledHeadingSection = styled.div`
-  max-width: 520px;
-  margin: auto 20px;
-  margin-bottom: auto;
-  font-size: 20px;
-
-  @media (max-width: 767px) {
-    width: 100%;
-  }
-`
-
-const StyledProductDescription = styled.h4`
-  font-weight: 500;
-  font-size: 22px;
-  text-align: center;
-  color: ${({ theme, color }) => color || theme.colors.c26};
-`
-
-const StyledSmallText = styled.span`
-  font-size: 16px;
-  font-weight: 500;
-  margin: 20px 0;
-  ${({ isUpperCase }) => isUpperCase && 'text-transform: uppercase;'}
-`
-
-const StyledFeatureDescription = styled.div`
-  font-size: 1rem;
-  margin-bottom: 20px;
-  width: 100%;
-  text-align: left;
-  color: ${({ color, theme }) => color || theme.colors.c26};
-`
-
-const StyledTechnologyDescriptionWrapper = styled.div`
-  min-height: ${({ isMobile }) => (isMobile ? '150px' : '100px')};
-  display: flex;
-  flex-direction: column;
-  margin: 20px 20px 0;
-  max-width: 800px;
-  color: ${({ color, theme }) => color || theme.colors.c2};
-`
-
-const StyledGrid = styled.div`
-  display: grid;
-  grid-gap: 10px;
-  background-color: ${({ theme }) => theme.colors.c3};
-  width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
-  grid-gap: 1rem;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  border-top: 1px solid white;
-`
-
-const StyledCell = styled.div`
-  color: ${({ theme }) => theme.colors.c2};
-  margin: 20px;
-`
-
-const IconContainer = styled.div`
-  margin: auto 10px;
-  width: 40px;
-  height: auto;
-
-  @media (max-width: 960px) {
-    flex-shrink: 0;
-  }
-`
-
-const StyledHeaderText = styled.div`
-  margin: 0 10px 5px 10px;
-  text-align: left;
-`
-
-const Illustration = styled.img`
-  margin: 0 20px;
-  width: 50%;
-  height: 50%;
-  max-width: 520px;
-`
 
 const defaultTechnologyDescription =
   'Strove.io represents each environment as a Docker container built from a shared image. This lets you code in seconds, on any computer and forget that `it works on my machine` issue ever existed.'
