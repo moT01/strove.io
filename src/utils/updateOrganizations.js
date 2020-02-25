99fd1abebdf61c6d82899a6fb20fe98caaf23120
AdamZaczek <adamzaczekul@gmail.com>
2020-02-25 23:53:10 +0100

    Do not fetch organization if there is no token

--- src/utils/updateOrganizations.js
+++ src/utils/updateOrganizations.js
@@ -2,11 +2,14 @@ import { MY_ORGANIZATIONS } from 'queries'
 
 import { query } from './graphql'
 
-export default () =>
-  localStorage.getItem('token') &&
-  query({
-    name: 'myOrganizations',
-    storeKey: 'myOrganizations',
-    query: MY_ORGANIZATIONS,
-    fetchPolicy: 'network-only',
-  })
+export default () => async dispatch =>
+  localStorage.getItem('token')
+    ? dispatch(
+        query({
+          name: 'myOrganizations',
+          storeKey: 'myOrganizations',
+          query: MY_ORGANIZATIONS,
+          fetchPolicy: 'network-only',
+        })
+      )
+    : undefined
