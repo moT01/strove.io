3516591fb34657d8b359026076858823806f7633
MateuszOlek <olek.mateusz@gmail.com>
2020-02-26 17:29:14 +0100

    UpdateOrganizations utils works better

--- src/utils/updateOrganizations.js
+++ src/utils/updateOrganizations.js
@@ -2,15 +2,24 @@ import { MY_ORGANIZATIONS } from 'queries'
 
 import { query } from './graphql'
 
-export default ({ onSuccess = () => null } = {}) => dispatch =>
-  localStorage.getItem('token')
-    ? dispatch(
-        query({
-          name: 'myOrganizations',
-          storeKey: 'myOrganizations',
-          query: MY_ORGANIZATIONS,
-          fetchPolicy: 'network-only',
-          onSuccess: data => onSuccess(data),
-        })
-      )
-    : undefined
+export default ({ onSuccess = () => null } = {}) => dispatch => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    dispatch(
+      query({
+        context: {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'User-Agent': 'node',
+          },
+        },
+        name: 'myOrganizations',
+        storeKey: 'myOrganizations',
+        query: MY_ORGANIZATIONS,
+        fetchPolicy: 'network-only',
+        onSuccess: data => onSuccess(data),
+      })
+    )
+  }
+  return undefined
+}
