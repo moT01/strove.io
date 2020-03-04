ed7db2020673533f3275731b22f36cb62938b71a
adamzaczek <adamzaczekul@gmail.com>
2020-03-05 00:32:36 +0100

    Style updates

--- src/pages/welcome/teamMembers.js
+++ src/pages/welcome/teamMembers.js
@@ -103,32 +103,6 @@ const TableRow = styled.div`
   margin: 0 0 10px;
 `
 
-const RemoveButton = styled.button`
-  width: 30px;
-  height: 30px;
-  text-align: center;
-  box-shadow: inset 0px 1px 0px 0px #cf866c;
-  background: linear-gradient(to bottom, #d0451b 5%, #bc3315 100%);
-  background-color: #d0451b;
-  border-radius: 100%;
-  border: 1px solid #942911;
-  display: inline-block;
-  cursor: pointer;
-  color: #ffffff;
-  font-family: Arial;
-  font-size: 13px;
-  text-decoration: none;
-  text-shadow: 0px 1px 0px #854629;
-  :hover {
-    background: linear-gradient(to bottom, #bc3315 5%, #d0451b 100%);
-    background-color: #bc3315;
-  }
-  :active {
-    position: relative;
-    top: 1px;
-  }
-`
-
 const AddButton = styled.button`
   height: 30px;
   text-align: center;
@@ -169,10 +143,7 @@ const validatePort = values => {
 }
 
 const OrganizationName = ({ history }) => {
-  const [envVariables, setEnvVariables] = useState([
-    { name: 'Var one', value: 'random string' },
-    { name: 'What is love?', value: `Baby don't hurt me` },
-  ])
+  const [envVariables, setEnvVariables] = useState(['', ''])
 
   const myOrganizations = useSelector(selectors.api.getMyOrganizations)
   const dispatch = useDispatch()
@@ -188,9 +159,7 @@ const OrganizationName = ({ history }) => {
         <SectionTitle>Environment variables</SectionTitle>
         <SettingWrapper>
           <Formik
-            initialValues={{
-              envs: envVariables,
-            }}
+            initialValues={{ envs: ['', ''] }}
             validate={validatePort}
             render={({ values }) => (
               <StyledForm>
@@ -214,10 +183,7 @@ const OrganizationName = ({ history }) => {
                           <AddButton
                             type="button"
                             onClick={() => {
-                              // values.envs[values.envs.length - 1].name !== '' &&
-                              //   values.envs[values.envs.length - 1].value !==
-                              //     '' &&
-                              arrayHelpers.push({ name: '', value: '' })
+                              arrayHelpers.push('')
                             }}
                           >
                             + Add another
