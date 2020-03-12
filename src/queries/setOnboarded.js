f6af2134329e7785f0191110afff63ef55c98a76
AdamZaczek <adamzaczekul@gmail.com>
2020-03-12 01:31:04 +0100

    Fix setOnboarded query

--- src/queries/setOnboarded.js
+++ src/queries/setOnboarded.js
@@ -1,7 +1,12 @@
 import gql from 'graphql-tag'
+import { UserFragment } from './fragments/user'
 
 export default gql`
   mutation GitlabAuth($isOnboarded: Boolean!) {
-    setOnboarded(isOnboarded: $isOnboarded)
+    setOnboarded(isOnboarded: $isOnboarded) {
+      ...User
+    }
   }
+
+  ${UserFragment}
 `
