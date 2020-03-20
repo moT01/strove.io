04a8a6ea471a06e279f4d75508deeb49d56830c3
AdamZaczek <adamzaczekul@gmail.com>
2020-03-21 00:23:08 +0100

    Use new component

--- src/pages/home/banner.js
+++ src/pages/home/banner.js
@@ -5,6 +5,7 @@ import { isMobileOnly, isMobile } from 'react-device-detect'
 import { theme } from 'consts'
 
 import GetStarted from './getStarted'
+import BeforeAfter from './beforeAfter'
 import {
   StyledCellHeader,
   StyledSectionWrapper,
@@ -70,92 +71,7 @@ const Banner = () => (
         </StyledProductDescription>
       </SectionDivider>
     </StyledSectionWrapper>
-    <StyledSectionWrapper
-      isSecondary
-      padding="20px"
-      background="white"
-      isMobileOnly={isMobileOnly}
-    >
-      <SectionDivider isMobile={isMobile}>
-        <SectionWrapper isMobile={isMobile} padding="20px 10px">
-          <SmallSectionWrapper isMobile={isMobile}>
-            <StyledCell>
-              <StyledCellHeader>
-                <StyledH2 color={theme.colors.c3}>Before</StyledH2>
-              </StyledCellHeader>
-              <StyledHeaderText>
-                Common <b>it works on my machine</b> and <b>work from home</b>{' '}
-                problems are a lot of work
-              </StyledHeaderText>
-              <StyledCellHeader>
-                <StyledHeaderText>Virtual machines</StyledHeaderText>
-                <IconContainer>
-                  <img
-                    decoding="async"
-                    alt="docker"
-                    src={require('assets/vm.png')}
-                    style={{
-                      color: theme.colors.c1,
-                    }}
-                  />
-                </IconContainer>
-              </StyledCellHeader>
-              <StyledCellHeader>
-                <StyledHeaderText>Docker containers</StyledHeaderText>
-                <IconContainer>
-                  <img
-                    decoding="async"
-                    alt="docker"
-                    src={require('assets/docker.png')}
-                    style={{
-                      color: theme.colors.c1,
-                    }}
-                  />
-                </IconContainer>
-              </StyledCellHeader>
-              <StyledCellHeader>
-                <StyledHeaderText>Devops teams</StyledHeaderText>
-                <IconContainer>
-                  <img
-                    decoding="async"
-                    alt="docker"
-                    src={require('assets/people.png')}
-                    style={{
-                      color: theme.colors.c1,
-                    }}
-                  />
-                </IconContainer>
-              </StyledCellHeader>
-              <StyledCellHeader>
-                <StyledHeaderText>Leasing computers</StyledHeaderText>
-                <IconContainer>
-                  <img
-                    decoding="async"
-                    alt="docker"
-                    src={require('assets/macbooks.png')}
-                    style={{
-                      color: theme.colors.c1,
-                    }}
-                  />
-                </IconContainer>
-              </StyledCellHeader>
-            </StyledCell>
-          </SmallSectionWrapper>
-        </SectionWrapper>
-        <SectionWrapper isMobile={isMobile} padding="20px 10px">
-          <SmallSectionWrapper isMobile={isMobile}>
-            <StyledCell>
-              <StyledCellHeader>
-                <StyledH2 color={theme.colors.c3}>After</StyledH2>
-              </StyledCellHeader>
-              <StyledHeaderText>
-                Strove simplifies coding and managing programmer teams
-              </StyledHeaderText>
-            </StyledCell>
-          </SmallSectionWrapper>
-        </SectionWrapper>
-      </SectionDivider>
-    </StyledSectionWrapper>
+    <BeforeAfter />
     <StyledSectionWrapper
       isSecondary
       padding="20px"
