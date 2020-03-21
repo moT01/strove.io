42a3184a2fb0d8e107240607ec1d63dea730dd80
AdamZaczek <adamzaczekul@gmail.com>
2020-03-21 18:07:24 +0100

    Fix keys for invited userrs

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -375,7 +375,7 @@ const Dashboard = ({ history }) => {
                                     )}
                                     {(isOwner || isOrganizationOwner) &&
                                       team?.invited?.map(member => (
-                                        <RowWrapper key={member.name}>
+                                        <RowWrapper key={member.email}>
                                           <Divider columnOnMobile>
                                             <VerticalDivider>
                                               <UserPhoto
