93b5a0946ef077fe5d3637854cd48fbfbbb1c911
AdamZaczek <adamzaczekul@gmail.com>
2020-03-21 17:35:33 +0100

    Cleanup

--- src/pages/welcome/teamMembers.js
+++ src/pages/welcome/teamMembers.js
@@ -1,24 +1,12 @@
 import React, { memo } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import styled from 'styled-components/macro'
 import { withRouter } from 'react-router-dom'
-import * as Yup from 'yup'
 
 import { InviteMembersForm } from 'components'
-import { selectors } from 'state'
 
 import OnboardingContainer from './onboardingContainer'
 import { Title, SkipForNow } from './styled'
 
-const ColumnWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
 const TeamMembers = ({ history }) => {
-  const myOrganizations = useSelector(selectors.api.getMyOrganizations)
-  const dispatch = useDispatch()
-
   return (
     <OnboardingContainer>
       <>
