18b50def8d5d78dbf8ffb8e975e0b2cdfd0fb7d0
AdamZaczek <adamzaczekul@gmail.com>
2020-03-22 21:03:28 +0100

    Update query

--- src/queries/startCollaborationProject.js
+++ src/queries/startCollaborationProject.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 export default gql`
-  mutation StartCollaborationProject($projectId: ID!, $userId: ID!) {
-    startCollaborationProject(projectId: $projectId, userId: $userId)
+  mutation StartCollaborationProject($projectId: ID!, $teamId: ID!) {
+    startCollaborationProject(projectId: $projectId, teamId: $teamId)
   }
 `
