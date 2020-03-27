2adbd4336cee9df919247486ee4e17d2b499f30d
AdamZaczek <adamzaczekul@gmail.com>
2020-03-27 22:41:09 +0100

    Fix width

--- src/pages/blog/firstPost.js
+++ src/pages/blog/firstPost.js
@@ -7,7 +7,7 @@ import { SEO, Header } from 'components'
 
 const TextWell = styled.div`
   color: ${({ theme }) => theme.colors.c3};
-  width: 60vw;
+  max-width: 640px;
   margin: 40px 20px;
   padding: 20px;
   flex-direction: column;
@@ -17,10 +17,6 @@ const TextWell = styled.div`
   text-justify: inter-word;
   background-color: ${({ theme }) => theme.colors.c2};
   font-size: 20px;
-
-  @media (max-width: 1366px) {
-    width: 100%;
-  }
 `
 
 const StyledH1 = styled.h1`
@@ -33,6 +29,14 @@ const Legal = () => (
     <Header />
     <TextWell>
       <StyledH1>Teaching code online</StyledH1>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+      <br />
       <Link to="/">Go back to the homepage</Link>
     </TextWell>
     <Footer />
