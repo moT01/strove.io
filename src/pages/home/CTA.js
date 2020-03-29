db010db0cbc75f10725a20f0dc6bd694e330466e
adamzaczek <adamzaczekul@gmail.com>
2020-03-29 21:28:59 +0200

    Position map

--- src/pages/home/CTA.js
+++ src/pages/home/CTA.js
@@ -11,11 +11,41 @@ const StyledCTAWrapper = styled.div`
   justify-content: center;
   align-self: center;
   margin-top: 20px;
-  background: ${({ theme }) => theme.colors.c3};
+  /* background: ${({ theme }) => theme.colors.c2};
+  background-repeat: no-repeat;
+  /* background-size: 300px 100px; */
+
+  ::before {
+    content: "";
+    position: absolute;
+    cursor: pointer;
+    width: 800px;
+    height: 400px;
+    background: ${({ theme }) => theme.colors.c2};
     background-repeat: no-repeat;
-  background-size: 300px 100px;
-  /* background-image: url(${process.env.PUBLIC_URL + '/assets/map.png'}); */
+    background-color: rgba(240, 240, 240, 0.1);
     background-image: url(${Map});
+    opacity: 0.1;
+}
+
+  /* &:after
+   {
+    content: '';
+    background: url(image.jpg);
+    opacity: 0.5;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: absolute;
+    z-index: -1;
+    background: ${({ theme }) => theme.colors.c2};
+    background-repeat: no-repeat;
+    background-color: rgba(240, 240, 240, 0.1);
+    background-image: url(${Map});
+    opacity: 0.2;
+    content: "blah";
+  } */
 `
 
 const StyledButtonWrapper = styled.div`
