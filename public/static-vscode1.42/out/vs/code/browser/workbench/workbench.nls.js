e4a476bd348400f0e165a00888065bbc9515b433
adamzaczek <adamzaczekul@gmail.com>
2020-04-04 01:38:56 +0200

    Add vscode-1.39 assets

---
+++ public/static-vscode1.42/out/vs/code/browser/workbench/workbench.nls.js
@@ -0,0 +1,11 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+/*---------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/code/browser/workbench/workbench.nls", {
+	"vs/code/browser/workbench/workbench": [
+		"Open in Desktop"
+	]
+});
