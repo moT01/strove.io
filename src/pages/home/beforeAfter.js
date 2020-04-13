d3ed93cc58255530eebf2ea4d81bce52419b2dbf
AdamZaczek <adamzaczekul@gmail.com>
2020-04-13 15:57:59 +0200

    Update beforeAfter section

--- src/pages/home/beforeAfter.js
+++ src/pages/home/beforeAfter.js
@@ -10,102 +10,12 @@ import {
   IconContainer,
   SectionWrapper,
   SmallSectionWrapper,
-  StyledCell,
   StyledH2,
   StyledBeforeText,
   BeforeContainer,
-  BeforeAfterSection,
   DemoImage,
   StyledSectionWrapper,
 } from './styled'
-
-const Before = () => (
-  <BeforeAfterSection
-    isSecondary
-    padding="20px"
-    background="white"
-    isMobileOnly={isMobileOnly}
-  >
-    <SectionDivider isMobile={isMobile}>
-      <SectionWrapper isMobile={isMobile} padding="20px 10px">
-        <SmallSectionWrapper isMobile={isMobile}>
-          <StyledCell>
-            <BeforeItem>
-              <StyledH2 color={theme.colors.c3}>Before</StyledH2>
-            </BeforeItem>
-            <BeforeContainer>
-              <BeforeItem>
-                <StyledBeforeText>Virtual machines</StyledBeforeText>
-                <IconContainer>
-                  <img
-                    decoding="async"
-                    alt="docker"
-                    src={require('assets/vm.png')}
-                    style={{
-                      color: theme.colors.c1,
-                    }}
-                  />
-                </IconContainer>
-              </BeforeItem>
-              <BeforeItem>
-                <StyledBeforeText>Docker containers</StyledBeforeText>
-                <IconContainer>
-                  <img
-                    decoding="async"
-                    alt="docker"
-                    src={require('assets/docker.png')}
-                    style={{
-                      color: theme.colors.c1,
-                    }}
-                  />
-                </IconContainer>
-              </BeforeItem>
-              <BeforeItem>
-                <StyledBeforeText>Devops teams</StyledBeforeText>
-                <IconContainer>
-                  <img
-                    decoding="async"
-                    alt="docker"
-                    src={require('assets/people.png')}
-                    style={{
-                      color: theme.colors.c1,
-                    }}
-                  />
-                </IconContainer>
-              </BeforeItem>
-              <BeforeItem>
-                <StyledBeforeText>Leasing computers</StyledBeforeText>
-                <IconContainer>
-                  <Icon
-                    type="laptop"
-                    style={{ color: theme.colors.c3, fontSize: 40 }}
-                  />
-                </IconContainer>
-              </BeforeItem>
-            </BeforeContainer>
-          </StyledCell>
-        </SmallSectionWrapper>
-      </SectionWrapper>
-      <SectionWrapper isMobile={isMobile} padding="20px 10px">
-        <SmallSectionWrapper isMobile={isMobile}>
-          <StyledCell>
-            <BeforeItem>
-              <StyledH2 color={theme.colors.c3}>After</StyledH2>
-            </BeforeItem>
-            <BeforeContainer>
-              <DemoImage
-                decoding="async"
-                shadowOpacity="0.1"
-                src={require('assets/liveshare.png')}
-                alt="teams image"
-              />
-            </BeforeContainer>
-          </StyledCell>
-        </SmallSectionWrapper>
-      </SectionWrapper>
-    </SectionDivider>
-  </BeforeAfterSection>
-)
 
 const BeforeAfter = () => (
   <StyledSectionWrapper
