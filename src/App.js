26063c6090b50b634d1a94cdb63fd2f6ac41141b
AdamZaczek <adamzaczekul@gmail.com>
2020-04-15 23:31:11 +0200

    Fix login problems related to waiting for local storage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 // import Reactotron from 'reactotron-react-js'
 // import { reactotronRedux } from 'reactotron-redux'
 import { Route } from 'react-router-dom'
-import { persistStore } from 'redux-persist'
 
 import { WithTracker } from 'components'
 
 import Wrapper from './wrapper'
 import Routes from './routes'
-import store from './store'
 
 // if (process.env.NODE_ENV !== 'production') {
 //   Reactotron.configure()
@@ -16,25 +14,10 @@ import store from './store'
 //     .connect()
 // }
 
-export let persistor
-
-const Strove = () => {
-  const [isRehydrating, setIsRehydrating] = useState(true)
-  useEffect(() => {
-    persistor = persistStore(store, {}, () => {
-      setIsRehydrating(false)
-    })
-  }, [])
-
-  if (isRehydrating) {
-    return <div></div>
-  }
-
-  return (
-    <Wrapper>
-      <Route component={WithTracker(Routes, {})} />
-    </Wrapper>
-  )
-}
+const Strove = () => (
+  <Wrapper>
+    <Route component={WithTracker(Routes, {})} />
+  </Wrapper>
+)
 
 export default Strove
