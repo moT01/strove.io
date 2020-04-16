bb879f1dbe48a43b63bce735027d19eb2aae01c1
AdamZaczek <adamzaczekul@gmail.com>
2020-04-16 21:46:41 +0200

    Remove reactotron redundant leftovers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react'
-// import Reactotron from 'reactotron-react-js'
-// import { reactotronRedux } from 'reactotron-redux'
 import { Route } from 'react-router-dom'
 
 import { WithTracker } from 'components'
 
 import Wrapper from './wrapper'
 import Routes from './routes'
-
-// if (process.env.NODE_ENV !== 'production') {
-//   Reactotron.configure()
-//     .use(reactotronRedux())
-//     .connect()
-// }
 
 const Strove = () => (
   <Wrapper>
