6e6712ef5a99c7c2f597779d1cd35230ddef673a
AdamZaczek <adamzaczekul@gmail.com>
2020-04-19 15:47:45 +0200

    Remove redundant variables

--- src/components/addProjectModals.js
+++ src/components/addProjectModals.js
@@ -1,6 +1,5 @@
 import React, { memo } from 'react'
 import styled, { keyframes, css } from 'styled-components/macro'
-import { Link } from 'react-router-dom'
 import { isMobileOnly, isTablet } from 'react-device-detect'
 import { useDispatch } from 'react-redux'
 
@@ -9,7 +8,6 @@ import Modal from './modal'
 import { Github, Gitlab, Bitbucket } from 'components/svgs'
 import { actions } from 'state'
 import StroveButton from 'components/stroveButton.js'
-import { getWindowPathName } from 'utils'
 
 const REACT_APP_GITHUB_CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID
 const REACT_APP_GITLAB_CLIENT_ID = process.env.REACT_APP_GITLAB_CLIENT_ID
@@ -85,53 +83,6 @@ const StyledAnchor = styled.a`
     height: 100%;
     margin-left: 5px;
   }
-
-  :focus {
-    outline: 0;
-  }
-
-  &:disabled {
-    opacity: 0.4;
-  }
-
-  ${props =>
-    !props.disabled &&
-    css`
-      animation: ${ButtonFadeIn} 1s ease-out;
-      cursor: pointer;
-      &:hover {
-        opacity: 1;
-        transform: translateY(-3px);
-        box-shadow: 0 12px 12px -13px ${({ theme }) => theme.colors.c1};
-      }
-    `}
-`
-
-const StyledLink = styled(Link)`
-  display: flex;
-  flex-direction: row;
-  height: auto;
-  width: 100%;
-  min-width: 70px;
-  margin: 5px;
-  padding: 10px;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  background-color: ${({ primary, theme }) =>
-    primary ? theme.colors.c1 : theme.colors.c2};
-  border-width: 1px;
-  border-style: solid;
-  font-size: 0.9rem;
-  color: ${({ primary, theme }) =>
-    primary ? theme.colors.c2 : theme.colors.c1};
-  border-radius: 5px;
-  border-color: ${({ theme }) => theme.colors.c1};
-  box-shadow: 0 10px 10px -15px ${({ theme }) => theme.colors.c1};
-  text-decoration: none;
-  transition: all 0.2s ease;
-  animation: ${FadeIn} 0.5s ease-out;
-  opacity: 0.9;
 
   :focus {
     outline: 0;
