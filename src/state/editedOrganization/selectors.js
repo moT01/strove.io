6af8d444d7a4f6cab191a42473091942d6ee87b4
AdamZaczek <adamzaczekul@gmail.com>
2020-04-19 21:00:30 +0200

    Make editerOrganization fail safe

--- src/state/editedOrganization/selectors.js
+++ src/state/editedOrganization/selectors.js
@@ -4,7 +4,7 @@ import { getOwnedOrganizations } from '../api/selectors'
 
 export const getEditedOrganization = state =>
   getOr(null, ['editedOrganization', 'organization'])(state) ||
-  getOwnedOrganizations(state)[0]
+  getOwnedOrganizations(state)?.[0]
 
 export const getEditedTeam = state =>
   getOr(null, ['editedOrganization', 'team'])(state)  || getOwnedOrganizations(state)[0]?.teams?.[0]
