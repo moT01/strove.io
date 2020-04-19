f70b331039c647cc07ebe2edcf036c1c17f94158
AdamZaczek <adamzaczekul@gmail.com>
2020-04-19 15:15:45 +0200

    Support liveshare projects in stopProject

--- src/utils/stopProject.js
+++ src/utils/stopProject.js
@@ -1,16 +1,25 @@
 import { mutation, updateOrganizations } from 'utils'
-import { STOP_PROJECT } from 'queries'
+import { STOP_PROJECT, STOP_LIVE_SHARE } from 'queries'
 import store from 'store'
 import { selectors } from 'state'
 
-const handleStopProject = ({ id }) => {
+const handleStopProject = ({ id, isLiveshare = false }) => {
   const state = store.getState()
   const dispatch = store.dispatch
   const currentProject = selectors.api.getCurrentProject(state)
 
-  console.log('currentProject', currentProject)
+  if (isLiveshare) {
+    return dispatch(
+      mutation({
+        name: 'stopLiveShare',
+        mutation: STOP_LIVE_SHARE,
+        variables: { projectId: currentProject.id },
+        onSuccessDispatch: updateOrganizations,
+      })
+    )
+  }
 
-  dispatch(
+  return dispatch(
     mutation({
       name: 'stopProject',
       mutation: STOP_PROJECT,
