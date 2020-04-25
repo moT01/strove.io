f1cd7b09b977294cf546fb32a3e898598541ed8f
AdamZaczek <adamzaczekul@gmail.com>
2020-04-25 17:22:35 +0200

    Cleanup

--- src/pages/home/beforeAfter.js
+++ src/pages/home/beforeAfter.js
@@ -1,7 +1,6 @@
 import React, { memo, useState } from 'react'
 import { isMobileOnly, isMobile } from 'react-device-detect'
 import { Icon } from 'antd'
-// import featuresSlides from 'assets/featuresSlides.mp4'
 
 import { theme } from 'consts'
 
@@ -14,7 +13,6 @@ import {
   StyledH2,
   StyledBeforeText,
   BeforeContainer,
-  DemoImage,
   StyledSectionWrapper,
   StyledVideo,
 } from './styled'
@@ -89,17 +87,7 @@ const BeforeAfter = () => {
         <SectionWrapper isMobile={isMobile} padding="20px 10px">
           <SectionWrapper isMobile={isMobile} padding="20px 10px">
             <SectionWrapper isMobile={isMobile}>
-              {/* <BeforeItem>
-              <StyledH2 color={theme.colors.c3}>After</StyledH2>
-            </BeforeItem> */}
-              {/* <DemoImage
-              decoding="async"
-              shadowOpacity="0.1"
-              src={require('assets/liveshare.png')}
-              alt="teams image"
-            /> */}
               <StyledVideo
-                controls
                 loop
                 autoPlay
                 preload="none"
