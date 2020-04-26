89115f9644833aa7b8cb4fca401f8d2071fab5cb
AdamZaczek <adamzaczekul@gmail.com>
2020-04-27 01:58:35 +0200

    Fix footer styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -36,7 +36,6 @@ const Column = styled.div`
 
 const ColumnTitle = styled.h3`
   font-size: 1.3rem;
-  font-weight: 300;
   color: ${({ theme }) => theme.colors.c2};
   margin: 10px 0;
 `
@@ -48,7 +47,6 @@ const StyledLink = styled(Link)`
 
 const LinkText = styled.h3`
   font-size: 0.9rem;
-  font-weight: 200;
   margin: 0 0 10px 0;
   cursor: pointer;
 `
